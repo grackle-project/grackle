1553796cb708de8d6f3134a291e9bbea11091811
Matthew Turk <matthewturk@gmail.com>
2013-02-11 15:03:01 -0500

    yt interface for pygrackle

--- src/python/pygrackle/fluid_container.py
+++ src/python/pygrackle/fluid_container.py
@@ -29,3 +29,53 @@ class FluidContainer(dict):
     @property
     def density_fields(self):
         return _fluid_names[self.chemistry_data.primordial_chemistry]
+
+class FieldNotFound(Exception):
+    def __init__(self, field):
+        self.field = field
+
+    def __repr__(self):
+        return "Field '%s' not found!" % (self.field)
+
+class NotAGrid(Exception):
+    def __repr__(self):
+        return "This routine needs a yt grid."
+
+_grackle_to_yt = {
+    "density": "Density",
+    "HI": "HI_Density",
+    "HII": "HII_Density",
+    "HM": "HM_Density",
+    "HeI": "HeI_Density",
+    "HeII": "HeII_Density",
+    "HeIII": "HeIII_Density",
+    "H2I": "H2I_Density",
+    "H2II": "H2II_Density",
+    "DI": "DI_Density",
+    "DII": "DII_Density",
+    "HDI": "HDI_Density",
+    "de": "Electron_Density",
+    "metal": "Metal_Density",
+    "x-velocity": "x-velocity",
+    "y-velocity": "y-velocity",
+    "z-velocity": "z-velocity",
+    "energy": "Thermal_Energy",
+}
+
+_yt_to_grackle = dict((b, a) for a, b in _grackle_to_yt.items())
+
+def grid_to_grackle(chemistry_data, grid):
+    if not hasattr(grid, ActiveDimensions):
+        raise RuntimeError
+    pf = grid.pf
+    fields = pf.h.derived_field_list + pf.h.field_list
+    ni = grid.ActiveDimensions[0]
+    fc = FluidContainer(chemistry_data, ni)
+    if any(_grackle_to_yt[f] not in fields
+           for f in fc):
+        raise FieldNotFound((f,_grackle_to_yt[f]))
+    for j in xrange(grid.ActiveDimensions[1]):
+        for k in xrange(grid.ActiveDimensions[2]):
+            for f in fc: # All the fields in yt
+                fc[f] = grid[_grackle_to_yt[f]][:,j,k]
+            yield fc
