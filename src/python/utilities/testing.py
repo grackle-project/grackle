341a7c68d62be770a7b91b3acb47829135ddbe0b
Britton Smith <brittonsmith@gmail.com>
2013-10-23 14:35:18 +0100

    Some updates to the testing framework, including moving testing imports to a separate file, updating the setup_fluid_container iterator to print out the least converged species, and adding a few more convenience functions.

---
+++ src/python/utilities/testing.py
@@ -0,0 +1,21 @@
+import numpy as np
+from numpy.testing import assert_array_equal, assert_almost_equal, \
+    assert_approx_equal, assert_array_almost_equal, assert_equal, \
+    assert_array_less, assert_string_equal, assert_array_almost_equal_nulp,\
+    assert_allclose, assert_raises
+
+def assert_rel_equal(a1, a2, decimals, err_msg='', verbose=True):
+    if isinstance(a1, np.ndarray):
+        assert(a1.size == a2.size)
+        # Mask out NaNs
+        a1[np.isnan(a1)] = 1.0
+        a2[np.isnan(a2)] = 1.0
+    elif np.any(np.isnan(a1)) and np.any(np.isnan(a2)):
+        return True
+    return assert_almost_equal(np.array(a1)/np.array(a2), 1.0, 
+                               decimals, err_msg=err_msg,
+                               verbose=verbose)
+
+def random_logscale(log_min, log_max, size=1):
+    log_val = (log_max - log_min) * np.random.random(size) + log_min
+    return np.power(10, log_val)
