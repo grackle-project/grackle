022154c9b186fdd6e873c48ef244194cd422d3c1
Britton Smith <brittonsmith@gmail.com>
2014-07-07 13:35:09 +0100

    Adding fortran callable cooling time calculator.

--- src/clib/calculate_cooling_time.c
+++ src/clib/calculate_cooling_time.c
@@ -170,3 +170,41 @@ int calculate_cooling_time(code_units *my_units, gr_float a_value,
  
   return SUCCESS;
 }
+
+int calculate_cooling_time_(gr_int *comoving_coordinates,
+                            gr_float *density_units, gr_float *length_units,
+                            gr_float *time_units, gr_float *velocity_units,
+                            gr_float *a_units, gr_float *a_value,
+                            gr_int *grid_rank, gr_int *grid_dimension,
+                            gr_int *grid_start, gr_int *grid_end,
+                            gr_float *density, gr_float *internal_energy,
+                            gr_float *x_velocity, gr_float *y_velocity, gr_float *z_velocity,
+                            gr_float *HI_density, gr_float *HII_density, gr_float *HM_density,
+                            gr_float *HeI_density, gr_float *HeII_density, gr_float *HeIII_density,
+                            gr_float *H2I_density, gr_float *H2II_density,
+                            gr_float *DI_density, gr_float *DII_density, gr_float *HDI_density,
+                            gr_float *e_density, gr_float *metal_density,
+                            gr_float *cooling_time)
+{
+
+  code_units my_units;
+  my_units.comoving_coordinates = *comoving_coordinates;
+  my_units.density_units = *density_units;
+  my_units.length_units = *length_units;
+  my_units.time_units = *time_units;
+  my_units.velocity_units = *velocity_units;
+  my_units.a_units = *a_units;
+
+  return calculate_cooling_time(&my_units, *a_value,
+                                *grid_rank, grid_dimension,
+                                grid_start, grid_end,
+                                density, internal_energy,
+                                x_velocity, y_velocity, z_velocity,
+                                HI_density, HII_density, HM_density,
+                                HeI_density, HeII_density, HeIII_density,
+                                H2I_density, H2II_density,
+                                DI_density, DII_density, HDI_density,
+                                e_density, metal_density,
+                                cooling_time);
+
+}
