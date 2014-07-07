573c9da3877c1e8c1abbb92d6baf101fe7db156c
Britton Smith <brittonsmith@gmail.com>
2014-07-07 13:42:58 +0100

    Adding gamma calculator.

--- src/clib/calculate_gamma.c
+++ src/clib/calculate_gamma.c
@@ -113,3 +113,38 @@ int calculate_gamma(code_units *my_units,
 
   return SUCCESS;
 }
+
+int calculate_gamma_(gr_int *comoving_coordinates,
+                     gr_float *density_units, gr_float *length_units,
+                     gr_float *time_units, gr_float *velocity_units,
+                     gr_float *a_units,
+                     gr_int *grid_rank, gr_int *grid_dimension,
+                     gr_float *density, gr_float *internal_energy,
+                     gr_float *HI_density, gr_float *HII_density, gr_float *HM_density,
+                     gr_float *HeI_density, gr_float *HeII_density, gr_float *HeIII_density,
+                     gr_float *H2I_density, gr_float *H2II_density,
+                     gr_float *DI_density, gr_float *DII_density, gr_float *HDI_density,
+                     gr_float *e_density, gr_float *metal_density,
+                     gr_float *my_gamma)
+
+{
+
+  code_units my_units;
+  my_units.comoving_coordinates = *comoving_coordinates;
+  my_units.density_units = *density_units;
+  my_units.length_units = *length_units;
+  my_units.time_units = *time_units;
+  my_units.velocity_units = *velocity_units;
+  my_units.a_units = *a_units;
+
+  return calculate_gamma(&my_units,
+                         *grid_rank, grid_dimension,
+                         density, internal_energy,
+                         HI_density, HII_density, HM_density,
+                         HeI_density, HeII_density, HeIII_density,
+                         H2I_density, H2II_density,
+                         DI_density, DII_density, HDI_density,
+                         e_density, metal_density,
+                         my_gamma);
+
+}
