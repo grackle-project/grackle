d86717a11c921721d22264a3bc20b7eea885dc89
Britton Smith <brittonsmith@gmail.com>
2014-09-21 21:25:54 +0100

    Removing definition of gr_int.

--- src/clib/grackle_types.h
+++ src/clib/grackle_types.h
@@ -19,14 +19,6 @@
 /
 ************************************************************************/
 
-#ifdef SMALL_INTS
-#define gr_int int
-#endif
-
-#ifdef LARGE_INTS
-#define gr_int long long
-#endif
-
 #ifdef CONFIG_BFLOAT_4
 #define gr_float float
 #endif
