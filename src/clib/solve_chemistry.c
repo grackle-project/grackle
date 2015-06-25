37e0c5c58ee23ea88110acb48e3afb98a8049a5f
Britton Smith <brittonsmith@gmail.com>
2015-06-25 15:56:55 +0100

    Threading the cloudy_data_new flag into solvers.

--- src/clib/solve_chemistry.c
+++ src/clib/solve_chemistry.c
@@ -70,7 +70,8 @@ extern void FORTRAN_NAME(solve_rate_cool_g)(
         double *priMMW, 
         long long *metGridRank, long long *metGridDim,
  	double *metPar1, double *metPar2, double *metPar3, 
- 	long long *metDataSize, double *metCooling, double *metHeating);
+ 	long long *metDataSize, double *metCooling, double *metHeating,
+        int *clnew);
 
 int _solve_chemistry(chemistry_data *my_chemistry,
                      code_units *my_units,
@@ -189,7 +190,8 @@ int _solve_chemistry(chemistry_data *my_chemistry,
     my_chemistry->cloudy_metal.grid_parameters[2],
     &my_chemistry->cloudy_metal.data_size,
     my_chemistry->cloudy_metal.cooling_data, 
-    my_chemistry->cloudy_metal.heating_data);
+    my_chemistry->cloudy_metal.heating_data,
+    &my_chemistry->cloudy_data_new);
 
   return SUCCESS;
 
