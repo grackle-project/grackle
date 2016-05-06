3be0e5529db03dfec5422e2aba4ba7a14f172170
Britton Smith <brittonsmith@gmail.com>
2016-05-06 14:58:02 +0100

    Returning tabulated temperature calculation because we need that.

--- src/clib/calculate_temperature.c
+++ src/clib/calculate_temperature.c
@@ -35,6 +35,17 @@ extern chemistry_data grackle_data;
  
 /* function prototypes */ 
 
+extern void FORTRAN_NAME(calc_temp_cloudy_g)(
+        gr_float *d, gr_float *e, gr_float *metal, gr_float *temperature,
+	int *in, int *jn, int *kn, int *iexpand, int *imetal,
+	int *is, int *js, int *ks, int *ie, int *je, int *ke,
+	double *aye, double *temstart, double *temend,
+	double *utem, double *uxyz, double *uaye, double *urho, double *utim,
+	double *gamma, double *fh,
+        long long *priGridRank, long long *priGridDim,
+        double *priPar1, double *priPar2, double *priPar3, 
+ 	long long *priDataSize, double *priMMW);
+
 int _calculate_pressure(chemistry_data *my_chemistry,
                         code_units *my_units, double a_value,
                         int grid_rank, int *grid_dimension,
@@ -47,12 +58,13 @@ int _calculate_pressure(chemistry_data *my_chemistry,
                         gr_float *e_density, gr_float *metal_density,
                         gr_float *pressure);
 
-int calculate_temperature_table(code_units *my_units, double a_value,
-                                int grid_rank, int *grid_dimension,
-                                int *grid_start, int *grid_end,
-                                gr_float *density, gr_float *internal_energy,
-                                gr_float *metal_density,
-                                gr_float *temperature);
+int _calculate_temperature_table(chemistry_data *my_chemistry,
+                                 code_units *my_units, double a_value,
+                                 int grid_rank, int *grid_dimension,
+                                 int *grid_start, int *grid_end,
+                                 gr_float *density, gr_float *internal_energy,
+                                 gr_float *metal_density,
+                                 gr_float *temperature);
  
 int _calculate_temperature(chemistry_data *my_chemistry,
                            code_units *my_units, double a_value,
@@ -144,6 +156,72 @@ int _calculate_temperature(chemistry_data *my_chemistry,
     temperature[i] = max(temperature[i], MINIMUM_TEMPERATURE);
   }
  
+  return SUCCESS;
+}
+
+int _calculate_temperature_table(chemistry_data *my_chemistry,
+                                 code_units *my_units, double a_value,
+                                 int grid_rank, int *grid_dimension,
+                                 int *grid_start, int *grid_end,
+                                 gr_float *density, gr_float *internal_energy,
+                                 gr_float *metal_density,
+                                 gr_float *temperature)
+{
+
+  if (!my_chemistry->use_grackle)
+    return SUCCESS;
+
+  if (my_chemistry->primordial_chemistry != 0) {
+    fprintf(stderr, "ERROR: this function requires primordial_chemistry set to 0.\n");
+    return FAIL;
+  }
+ 
+  /* Compute the size of the fields. */
+ 
+  int i, dim, size = 1;
+  for (dim = 0; dim < grid_rank; dim++)
+    size *= grid_dimension[dim];
+
+  /* Check for a metal field. */
+
+  int metal_field_present = TRUE;
+  if (metal_density == NULL)
+    metal_field_present = FALSE;
+
+  double co_length_units, co_density_units;
+  if (my_units->comoving_coordinates == TRUE) {
+    co_length_units = my_units->length_units;
+    co_density_units = my_units->density_units;
+  }
+  else {
+    co_length_units = my_units->length_units *
+      a_value * my_units->a_units;
+    co_density_units = my_units->density_units /
+      POW(a_value * my_units->a_units, 3);
+  }
+
+  /* Calculate temperature units. */
+
+  double temperature_units = mh * POW(my_units->velocity_units, 2) / kboltz;
+
+  FORTRAN_NAME(calc_temp_cloudy_g)(
+        density, internal_energy, metal_density, temperature,
+        grid_dimension, grid_dimension+1, grid_dimension+2,
+        &my_units->comoving_coordinates, &metal_field_present,
+        grid_start, grid_start+1, grid_start+2,
+        grid_end, grid_end+1, grid_end+2,
+        &a_value, &my_chemistry->TemperatureStart, &my_chemistry->TemperatureEnd,
+        &temperature_units, &co_length_units, &my_units->a_units, 
+        &co_density_units, &my_units->time_units,
+        &my_chemistry->Gamma, &my_chemistry->HydrogenFractionByMass,
+        &my_chemistry->cloudy_primordial.grid_rank,
+        my_chemistry->cloudy_primordial.grid_dimension,
+        my_chemistry->cloudy_primordial.grid_parameters[0],
+        my_chemistry->cloudy_primordial.grid_parameters[1],
+        my_chemistry->cloudy_primordial.grid_parameters[2],
+        &my_chemistry->cloudy_primordial.data_size,
+        my_chemistry->cloudy_primordial.mmw_data);
+
   return SUCCESS;
 }
 
