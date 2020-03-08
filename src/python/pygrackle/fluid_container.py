a370e28f14c3621c44a6b3672e4d5059135000cd
Britton Smith <brittonsmith@gmail.com>
2020-03-08 13:46:25 +0100

    Remove old gracke yt wrapper.

--- src/python/pygrackle/fluid_container.py
+++ src/python/pygrackle/fluid_container.py
@@ -3,7 +3,7 @@
 # Grackle python fluid container
 #
 #
-# Copyright (c) 2013, Enzo/Grackle Development Team.
+# Copyright (c) Enzo/Grackle Development Team. All rights reserved.
 #
 # Distributed under the terms of the Enzo Public Licence.
 #
@@ -50,11 +50,6 @@ _rad_trans_names = ['RT_heating_rate', 'RT_HI_ionization_rate',
 _extra_fields = {}
 _extra_fields[2] = ["H2_self_shielding_length"]
 _extra_fields[3] = _extra_fields[2] + []
-
-try:
-    xrange
-except NameError:
-    xrange = range
 
 class FluidContainer(dict):
     def __init__(self, chemistry_data, n_vals, dtype="float64",
@@ -157,78 +152,3 @@ class FluidContainer(dict):
 
     def solve_chemistry(self, dt):
         solve_chemistry(self, dt)
-
-class FieldNotFound(Exception):
-    def __init__(self, field):
-        self.field = field
-
-    def __str__(self):
-        return "Field '%s' not found!" % ((self.field, ))
-
-class NotAGrid(Exception):
-    def __str__(self):
-        return "This routine needs a yt grid."
-
-
-_grackle_to_yt = {
-    'density': ('gas', 'density'),
-    'HI': ('gas', 'H_p0_density'),
-    'HII': ('gas', 'H_p1_density'),
-    'HM': ('gas', 'HM_density'),
-    'HeI': ('gas', 'He_p0_density'),
-    'HeII': ('gas', 'He_p1_density'),
-    'HeIII': ('gas', 'He_p2_density'),
-    'H2I': ('gas', 'H2_p0_density'),
-    'H2II': ('gas', 'H2_p1_density'),
-    'DI': ('gas', 'D_p0_density'),
-    'DII': ('gas', 'D_p1_density'),
-    'HDI': ('gas', 'HD_p0_density'),
-    'de': ('gas', 'El_density'),
-    'metal': ('gas', 'metal_density'),
-    'dust': ('gas', 'dust_density'),
-    'x-velocity': ('gas', 'velocity_x'),
-    'y-velocity': ('gas', 'velocity_y'),
-    'z-velocity': ('gas', 'velocity_z'),
-    'energy': ('gas', 'thermal_energy'),
-}
-
-_skip = ("pressure", "temperature", "dust_temperature", "dust",
-         "cooling_time", "gamma", "mu", "nH", "mean_molecular_weight")
-
-_yt_to_grackle = dict((b, a) for a, b in _grackle_to_yt.items())
-
-_have_units = ["density", "energy", "pressure", "temperature", "velocity"]
-
-def _units(chemistry_data, fname):
-    for f in _have_units:
-        if f in fname[1]:
-            my_units = "%s_units" % f
-            units = getattr(chemistry_data, my_units)
-            return units
-    raise FieldNotFound(fname)
-
-def _needed_fields(fc):
-    cd = fc.chemistry_data
-    for f in sorted(fc):
-        if f in _skip: continue
-        f2 = _grackle_to_yt[f]
-        yield f, f2, _units(cd, f2)
-
-def grid_to_grackle(chemistry_data, grid, update = True):
-    if not hasattr(grid, "ActiveDimensions"):
-        raise RuntimeError
-    ds = grid.ds
-    fields = ds.derived_field_list + ds.field_list
-    ni = grid.ActiveDimensions[0]
-    fc = FluidContainer(chemistry_data, ni)
-    for f1, f2, conv in _needed_fields(fc):
-        if f2 not in fields:
-            raise FieldNotFound(f2)
-    for j in range(grid.ActiveDimensions[1]):
-        for k in range(grid.ActiveDimensions[2]):
-            for f1, f2, conv in _needed_fields(fc):
-                fc[f1][:] = grid[f2][:,j,k] / conv
-            yield fc
-            if not update: continue
-            for f1, f2, conv in _needed_fields(fc):
-                grid[f2][:,j,k] = fc[f1][:] * conv
