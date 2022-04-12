addd4e8ef34118155c0ede3922814faacfe51ea7
mabruzzo <matthewabruzzo@gmail.com>
2022-04-12 13:23:03 -0400

    modified calculate_gamma to respect grid_start and grid_end.

--- src/clib/calculate_gamma.c
+++ src/clib/calculate_gamma.c
@@ -31,6 +31,8 @@ int local_calculate_temperature(chemistry_data *my_chemistry,
                                 grackle_field_data *my_fields,
                                 gr_float *temperature);
 
+grackle_index_helper _build_index_helper(const grackle_field_data *my_fields);
+
 int local_calculate_gamma(chemistry_data *my_chemistry,
                           chemistry_data_storage *my_rates,
                           code_units *my_units,
@@ -41,15 +43,20 @@ int local_calculate_gamma(chemistry_data *my_chemistry,
   if (!my_chemistry->use_grackle)
     return SUCCESS;
  
-  int i, dim, size = 1;
-  for (dim = 0; dim < my_fields->grid_rank; dim++)
-    size *= my_fields->grid_dimension[dim];
+  const grackle_index_helper ind_helper = _build_index_helper(my_fields);
+  int outer_ind, i, j, k, index;
   
   /* If molecular hydrogen is not being used, just use monotonic.
      (this should not really be called, but provide it just in case). */
- 
-  for (i = 0; i < size; i++) {
-    my_gamma[i] = my_chemistry->Gamma;
+
+  for (outer_ind = 0; outer_ind < ind_helper.outer_ind_size; outer_ind++){
+    k = (outer_ind / ind_helper.num_j_inds) + ind_helper.k_start;
+    j = (outer_ind % ind_helper.num_j_inds) + ind_helper.j_start;
+
+    for (i = ind_helper.i_start; i <= ind_helper.i_end; i++) {
+      index = i + ind_helper.i_dim * (j + ind_helper.j_dim * k);
+      my_gamma[index] = my_chemistry->Gamma;
+    }
   }
  
   if (my_chemistry->primordial_chemistry > 1) {
@@ -68,39 +75,50 @@ int local_calculate_gamma(chemistry_data *my_chemistry,
     double x, nH2, number_density, GammaH2Inverse, 
       GammaInverse = 1 / (my_chemistry->Gamma - 1.0);
 
+    /* parallelize the k and j loops with OpenMP
+     * (these loops are flattened them for better parallelism) */
 #   ifdef _OPENMP
 #   pragma omp parallel for schedule( runtime ) \
-    private( x, nH2, number_density, GammaH2Inverse )
+    private( outer_ind, i, j, k, index, x, nH2, number_density, GammaH2Inverse )
 #   endif
-    for (i = 0; i < size; i++) {
+    for (outer_ind = 0; outer_ind < ind_helper.outer_ind_size; outer_ind++){
+
+      k = (outer_ind / ind_helper.num_j_inds) + ind_helper.k_start;
+      j = (outer_ind % ind_helper.num_j_inds) + ind_helper.j_start;
+
+      for (i = ind_helper.i_start; i <= ind_helper.i_end; i++) {
+	index = i + ind_helper.i_dim * (j + ind_helper.j_dim * k);
  
-      /* Compute relative number abundence of molecular hydrogen. */
+	/* Compute relative number abundence of molecular hydrogen. */
  
-      number_density =
-        0.25 * (my_fields->HeI_density[i] + my_fields->HeII_density[i] +
-                my_fields->HeIII_density[i]) +
-        my_fields->HI_density[i] + my_fields->HII_density[i] +
-        my_fields->HM_density[i] + my_fields->e_density[i];
+	number_density =
+	  0.25 * (my_fields->HeI_density[index] +
+		  my_fields->HeII_density[index] +
+		  my_fields->HeIII_density[index]) +
+	  my_fields->HI_density[index] + my_fields->HII_density[index] +
+	  my_fields->HM_density[index] + my_fields->e_density[index];
  
-      nH2 = 0.5 * (my_fields->H2I_density[i] + my_fields->H2II_density[i]);
+	nH2 = 0.5 * (my_fields->H2I_density[index] +
+		     my_fields->H2II_density[index]);
  
-      /* Only do full computation if there is a reasonable amount of H2.
-         The second term in GammaH2Inverse accounts for the vibrational
-         degrees of freedom. */
+	/* Only do full computation if there is a reasonable amount of H2.
+	   The second term in GammaH2Inverse accounts for the vibrational
+	   degrees of freedom. */
  
-      GammaH2Inverse = 0.5*5.0;
-      if (nH2 / number_density > 1e-3) {
-	x = 6100.0 / my_gamma[i];
-	if (x < 10.0)
-	  GammaH2Inverse = 0.5*(5 + 2.0 * x*x * exp(x)/POW(exp(x)-1.0,2));
-      }
+	GammaH2Inverse = 0.5*5.0;
+	if (nH2 / number_density > 1e-3) {
+	  x = 6100.0 / my_gamma[index];
+	  if (x < 10.0)
+	    GammaH2Inverse = 0.5*(5 + 2.0 * x*x * exp(x)/POW(exp(x)-1.0,2));
+	}
+
+	/* Add in H2. */
+
+	my_gamma[index] = 1.0 + (nH2 + number_density) /
+	  (nH2 * GammaH2Inverse + number_density * GammaInverse);
  
-      /* Add in H2. */
- 
-      my_gamma[i] = 1.0 + (nH2 + number_density) /
-        (nH2 * GammaH2Inverse + number_density * GammaInverse);
- 
-    } // end: loop over i
+      } // end: loop over i
+    } // end: loop over outer_ind
  
   } // end: if (my_chemistry->primordial_chemistry > 1)
 
