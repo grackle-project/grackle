be586e08ae0cb1ce5c6eaa4ffe50a2d77f030e5f
Matthew Abruzzo <matthewabruzzo@gmail.com>
2023-05-15 16:28:55 -0400

    cool1d_cloudy_g.c: removed internal heap allocations

--- src/clib/interop/cool1d_cloudy_g.c
+++ src/clib/interop/cool1d_cloudy_g.c
@@ -35,14 +35,6 @@ void cool1d_cloudy_g(
   const double inv_log10 = 1.0 / log(10.0);
   const double log10_tCMB = log10(comp2);
 
-  // local slices
-  double* log_n_h = malloc(sizeof(double)*in);
-  double* log_cool = malloc(sizeof(double)*in);
-  double* log_cool_cmb = malloc(sizeof(double)*in);
-  double* log_heat = malloc(sizeof(double)*in);
-  double* edot_met = malloc(sizeof(double)*in);
-  double* log10tem = malloc(sizeof(double)*in);
-
   // Calculate parameter value slopes
   const double dclPar[3] = {
      ((clPar1[clGridDim[0]-1] - clPar1[0]) / (double)(clGridDim[0] - 1)),
@@ -64,92 +56,95 @@ void cool1d_cloudy_g(
   for (int i = is + 1; i <= (ie + 1); i++) {
     if ( !itmask[i-1] ) { continue; }
 
-    log10tem[i-1] = logtem[i-1] * inv_log10;
-    log_n_h[i-1] = log10(rhoH[i-1] * dom); // Calculate proper log(n_H)
+    const double log10tem = logtem[i-1] * inv_log10;
+    const double log_n_h = log10(rhoH[i-1] * dom); // Calculate proper log(n_H)
+
+    double edot_met = 0.0;
+    double log_cool = 0.0;
+    double log_cool_cmb = 0.0;
+    double log_heat = 0.0;
 
     // Call interpolation functions to get heating and cooling
     if (clGridRank == 1) { // Interpolate over temperature.
-      interpolate_1d_g(log10tem[i-1], clGridDim,
+      interpolate_1d_g(log10tem, clGridDim,
                        clPar1, dclPar[0],
-                       clDataSize, clCooling, &(log_cool[i-1]));
-      // pow returns NaN when a negative base is raised to a non-integer power
-      edot_met[i-1] = -1.0 * pow(10.0,log_cool[i-1]);
+                       clDataSize, clCooling, &log_cool);
+      edot_met = -1.0 * pow(10.0,log_cool);
 
       // Ignore CMB term if T >> T_CMB
-      if ((icmbTfloor == 1) && ((log10tem[i-1] - log10_tCMB) < 2.0)) {
+      if ((icmbTfloor == 1) && ((log10tem - log10_tCMB) < 2.0)) {
         interpolate_1d_g(log10_tCMB, clGridDim,
                          clPar1, dclPar[0],
-                         clDataSize, clCooling, &(log_cool_cmb[i-1]));
-        edot_met[i-1] += pow(10.0,log_cool_cmb[i-1]);
+                         clDataSize, clCooling, &log_cool_cmb);
+        edot_met += pow(10.0,log_cool_cmb);
       }
 
       if (get_heat == 1){
-        interpolate_1d_g(log10tem[i-1], clGridDim,
+        interpolate_1d_g(log10tem, clGridDim,
                          clPar1, dclPar[0],
-                         clDataSize, clHeating, &(log_heat[i-1]));
-        edot_met[i-1] += pow(10.0,log_heat[i-1]);
+                         clDataSize, clHeating, &log_heat);
+        edot_met += pow(10.0,log_heat);
       }
+
     } else if ( clGridRank == 2) { // Interpolate over density & temperature.
-      interpolate_2d_g(log_n_h[i-1], log10tem[i-1],
+      interpolate_2d_g(log_n_h, log10tem,
                        clGridDim,
                        clPar1, dclPar[0],
                        clPar2, dclPar[1],
-                       clDataSize, clCooling, &(log_cool[i-1]));
-      // pow returns NaN when a negative base is raised to a non-integer power
-      edot_met[i-1] = -1.0 * pow(10.0,log_cool[i-1]);
+                       clDataSize, clCooling, &log_cool);
+      edot_met = -1.0 * pow(10.0,log_cool);
 
       // Ignore CMB term if T >> T_CMB
-      if ((icmbTfloor == 1) && ((log10tem[i-1] - log10_tCMB) < 2.0)) {
-        interpolate_2d_g(log_n_h[i-1], log10_tCMB,
+      if ((icmbTfloor == 1) && ((log10tem - log10_tCMB) < 2.0)) {
+        interpolate_2d_g(log_n_h, log10_tCMB,
                          clGridDim,
                          clPar1, dclPar[0],
                          clPar2, dclPar[1],
-                         clDataSize, clCooling, &(log_cool_cmb[i-1]));
-        edot_met[i-1] += pow(10.0,log_cool_cmb[i-1]);
+                         clDataSize, clCooling, &log_cool_cmb);
+        edot_met += pow(10.0,log_cool_cmb);
       }
 
       if (get_heat == 1){
-        interpolate_2d_g(log_n_h[i-1], log10tem[i-1],
+        interpolate_2d_g(log_n_h, log10tem,
                          clGridDim,
                          clPar1, dclPar[0],
                          clPar2, dclPar[1],
-                         clDataSize, clHeating, &(log_heat[i-1]));
-        edot_met[i-1] += pow(10.0,log_heat[i-1]);
+                         clDataSize, clHeating, &log_heat);
+        edot_met += pow(10.0,log_heat);
       }
 
     } else if ( clGridRank == 3) { // Interpolate over density, redshift,
                                    // & temperature.
-      interpolate_3dz_g(log_n_h[i-1], zr, log10tem[i-1],
+      interpolate_3dz_g(log_n_h, zr, log10tem,
                         clGridDim,
                         clPar1, dclPar[0],
                         clPar2, zindex,
                         clPar3, dclPar[2],
                         clDataSize, clCooling,
-                        end_int, &(log_cool[i-1]));
-      // pow returns NaN when a negative base is raised to a non-integer power
-      edot_met[i-1] = -1.0 * pow(10.0,log_cool[i-1]);
+                        end_int, &log_cool);
+      edot_met = -1.0 * pow(10.0,log_cool);
 
       // Ignore CMB term if T >> T_CMB
-      if ((icmbTfloor == 1) && ((log10tem[i-1] - log10_tCMB) < 2.0)) {
-        interpolate_3dz_g(log_n_h[i-1], zr, log10_tCMB,
+      if ((icmbTfloor == 1) && ((log10tem - log10_tCMB) < 2.0)) {
+        interpolate_3dz_g(log_n_h, zr, log10_tCMB,
                           clGridDim,
                           clPar1, dclPar[0],
                           clPar2, zindex,
                           clPar3, dclPar[2],
                           clDataSize, clCooling,
-                          end_int, &(log_cool_cmb[i-1]));
-        edot_met[i-1] += pow(10.0,log_cool_cmb[i-1]);
+                          end_int, &log_cool_cmb);
+        edot_met += pow(10.0,log_cool_cmb);
       }
 
       if (get_heat == 1){
-        interpolate_3dz_g(log_n_h[i-1], zr, log10tem[i-1],
+        interpolate_3dz_g(log_n_h, zr, log10tem,
                           clGridDim,
                           clPar1, dclPar[0],
                           clPar2, zindex,
                           clPar3, dclPar[2],
                           clDataSize, clHeating,
-                          end_int, &(log_heat[i-1]));
-        edot_met[i-1] += pow(10.0,log_heat[i-1]);
+                          end_int, &log_heat);
+        edot_met += pow(10.0,log_heat);
       }
 
     } else {
@@ -159,17 +154,9 @@ void cool1d_cloudy_g(
 
     // scale cooling by metallicity
     if (iZscale == 1) {
-      edot_met[i-1] = edot_met[i-1] * metallicity[i-1];
+      edot_met = edot_met * metallicity[i-1];
     }
 
-    edot[i-1] += (edot_met[i-1] * rhoH[i-1] * rhoH[i-1]);
+    edot[i-1] += (edot_met * rhoH[i-1] * rhoH[i-1]);
   }
-
-
-  free(log_n_h);
-  free(log_cool);
-  free(log_cool_cmb);
-  free(log_heat);
-  free(edot_met);
-  free(log10tem);
 }
