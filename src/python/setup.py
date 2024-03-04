c5c619eee8adb31e585f406ebe9e7d4f99e844d7
Matthew Abruzzo <matthewabruzzo@gmail.com>
2024-03-04 10:35:16 -0500

    modify pygrackle to support cmake builds
    This commit modifies the `setup.py` script to be able to build the
    pygrackle python package against versions of libgrackle that were
    compiled with the CMake build system.

--- src/python/setup.py
+++ src/python/setup.py
@@ -65,7 +65,7 @@ def _config_kw(package_locallib_copy, library_dir):
     return extension_kw, setup_pkgdata_kw
 
 
-# load in the configuration option
+# Process the env-variable configuration options:
 # - PYGRACKLE_PACKAGE_LOCALLIB:
 #   -> should be set to a value of 0 or 1
 #   -> in both cases, the copy of the libgrackle shared library from the
@@ -78,13 +78,57 @@ def _config_kw(package_locallib_copy, library_dir):
 #      shared library is NOT packaged with the python module. At runtime when
 #      extension module is loaded, we depend on the operating system's dynamic
 #      linker to search for the libgrackle shared library (e.g. first in paths
-#      given by LD_LIBRARY_PATH and then at other standard sytem locations)
-package_locallib_copy = _getenv_bool("PYGRACKLE_PACKAGE_LOCALLIB", False)
+#      given by LD_LIBRARY_PATH and then at other standard system locations)
+# - PYGRACKLE_CMAKE_BUILD_DIR
+#   -> this variable should be used when you want to indicate that the python
+#      extension module should be built against a copy of libgrackle that was
+#      constructed using cmake
+#   -> in more detail, this should specify the cmake build directory. A common
+#      choice is "../../build"
+package_locallib_copy = _getenv_bool("PYGRACKLE_PACKAGE_LOCALLIB", None)
+grackle_build_dir = os.getenv("PYGRACKLE_CMAKE_BUILD_DIR", "")
 
-library_dir="../clib/.libs/"
+# handle different choices of grackle_build_dir
+if grackle_build_dir == "": # traditional in-source build
+    TRADITIONAL_IN_SOURCE_BUILD_MACRO = '1'
+    library_dir="../clib/.libs/"
+    include_dirs = ["../clib"]
+    if package_locallib_copy is None: # fallback to historic behavior
+        package_locallib_copy = False
+
+else: # CMAKE-based out-of-source build
+    abs_grackle_build_dir = os.path.abspath(
+        path = os.path.expandvars(os.path.expanduser(grackle_build_dir))
+    )
+    TRADITIONAL_IN_SOURCE_BUILD_MACRO = '0'
+    library_dir=os.path.join(abs_grackle_build_dir, "src/clib")
+
+    if not os.path.isdir(abs_grackle_build_dir):
+        raise RuntimeError(f"{grackle_build_dir} is not a directory")
+
+    # unlike the traditional in-source build, the cmake approach puts the
+    # the generated headers in a different location
+    # -> therefore, we specify location where both sets of headers are found
+    # -> to be safe, ensure that there are no autogened files from alt approach
+    include_dirs = ["../clib", os.path.join(abs_grackle_build_dir, "src/clib")]
+    # don't bother checking result code. This will fail if we've never built
+    # grackle (if the depend file is missing
+    subprocess.run(["make", "clean_autogen"], cwd = "../clib")
+
+    if package_locallib_copy is None:
+        package_locallib_copy = True
+    elif package_locallib_copy == False:
+        raise ValueError("we haven't tested this configuration.")
 
 if package_locallib_copy and (_find_solib_basename(library_dir) is None):
-    raise RuntimeError(f"no shared library found in {library_dir!r}")
+    raise RuntimeError(
+        f"no shared library found in {library_dir!r}. If using cmake, did you "
+        "remember to: "
+        "1. configure the build with -DBUILD_SHARED_LIBS=ON AND "
+        "2. actually execute the build after configuring it?"
+        "3. use the PYGRACKLE_CMAKE_BUILD_DIR variable to tell setup.py where "
+        "   libgrackle was built"
+    )
 
 # for added safety, remove copies of the libgrackle shared library that were
 # previously copied into the python directory
@@ -98,9 +142,10 @@ cython_extensions = [
     Extension(
         "pygrackle.grackle_wrapper",
         ["pygrackle/grackle_wrapper.pyx"],
-        include_dirs=["../clib"],
+        include_dirs=include_dirs,
         define_macros=[
             ("CONFIG_BFLOAT_8", True),
+            ("TRADITIONAL_IN_SOURCE_BUILD", TRADITIONAL_IN_SOURCE_BUILD_MACRO),
         ],
         **extension_kw
     ),
