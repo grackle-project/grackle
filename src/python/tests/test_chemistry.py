53eff1ec748586aca86ef6e5a5454cc903c66ffa
Britton Smith <brittonsmith@gmail.com>
2024-08-06 16:23:42 +0100

    A bit more refactoring of test_chemistry.

--- src/python/tests/test_chemistry.py
+++ src/python/tests/test_chemistry.py
@@ -25,25 +25,28 @@ from pygrackle.utilities.testing import \
     assert_rel_equal, \
     assert_array_less
 
+_parameters = {
+    "use_grackle": 1,
+    "with_radiative_cooling": 0,
+    "primordial_chemistry": 1,
+    "metal_cooling": 1,
+    "UVbackground": 1,
+    "grackle_data_file": os.path.join(grackle_data_dir, "CloudyData_UVB=HM2012.h5")
+}
+
 
 def test_proper_comoving_units():
     """
     Make sure proper and comoving units systems give the same answer.
     """
 
-    data_file_path = os.path.join(grackle_data_dir, "CloudyData_UVB=HM2012.h5")
-
     my_random_state = np.random.RandomState(7921)
     for current_redshift in [0., 1., 3., 6., 9.]:
 
         # comoving units
         chem_c = chemistry_data()
-        chem_c.use_grackle = 1
-        chem_c.with_radiative_cooling = 0
-        chem_c.primordial_chemistry = 1
-        chem_c.metal_cooling = 1
-        chem_c.UVbackground = 1
-        chem_c.grackle_data_file = data_file_path
+        for par, val in _parameters.items():
+            setattr(chem_c, par, val)
         metal_fraction = 0.1 * chem_c.SolarMetalFractionByMass
         set_cosmology_units(chem_c,
                             current_redshift=current_redshift,
@@ -57,12 +60,8 @@ def test_proper_comoving_units():
 
         # proper units
         chem_p = chemistry_data()
-        chem_p.use_grackle = 1
-        chem_p.with_radiative_cooling = 0
-        chem_p.primordial_chemistry = 1
-        chem_p.metal_cooling = 1
-        chem_p.UVbackground = 1
-        chem_p.grackle_data_file = data_file_path
+        for par, val in _parameters.items():
+            setattr(chem_p, par, val)
         chem_p.comoving_coordinates = 0
         chem_p.a_units = 1.0
         chem_p.a_value = 1.0 / (1.0 + current_redshift) / chem_p.a_units
@@ -98,23 +97,19 @@ def test_proper_comoving_units_tabular():
     answer with tabular cooling.
     """
 
-    data_file_path = os.path.join(grackle_data_dir, "CloudyData_UVB=HM2012.h5")
-
     my_random_state = np.random.RandomState(19650909)
     for current_redshift in [0., 1., 3., 6., 9.]:
 
         # comoving units
         chem_c = chemistry_data()
-        chem_c.use_grackle = 1
-        chem_c.with_radiative_cooling = 0
-        chem_c.primordial_chemistry = 0
-        chem_c.metal_cooling = 1
-        chem_c.UVbackground = 1
-        chem_c.grackle_data_file = data_file_path
+        for par, val in _parameters.items():
+            setattr(chem_c, par, val)
+        metal_fraction = 0.1 * chem_c.SolarMetalFractionByMass
         set_cosmology_units(chem_c,
                             current_redshift=current_redshift,
                             initial_redshift=99.)
-        fc_c = setup_fluid_container(chem_c, converge=False)
+        fc_c = setup_fluid_container(chem_c, converge=False,
+                                     metal_mass_fraction=metal_fraction)
         fc_c.calculate_temperature()
         fc_c.calculate_cooling_time()
         t_sort_c = np.argsort(fc_c["temperature"])
@@ -122,12 +117,8 @@ def test_proper_comoving_units_tabular():
 
         # proper units
         chem_p = chemistry_data()
-        chem_p.use_grackle = 1
-        chem_p.with_radiative_cooling = 0
-        chem_p.primordial_chemistry = 0
-        chem_p.metal_cooling = 1
-        chem_p.UVbackground = 1
-        chem_p.grackle_data_file = data_file_path
+        for par, val in _parameters.items():
+            setattr(chem_p, par, val)
         chem_p.comoving_coordinates = 0
         chem_p.a_units = 1.0
         chem_p.a_value = 1.0 / (1.0 + current_redshift) / chem_p.a_units
@@ -140,7 +131,8 @@ def test_proper_comoving_units_tabular():
         chem_p.time_units = random_logscale(-2, 2, random_state=my_random_state)[0] * \
             chem_c.time_units
         chem_p.velocity_units = chem_p.length_units / chem_p.time_units
-        fc_p = setup_fluid_container(chem_p, converge=False)
+        fc_p = setup_fluid_container(chem_p, converge=False,
+                                     metal_mass_fraction=metal_fraction)
         fc_p.calculate_temperature()
         fc_p.calculate_cooling_time()
         t_sort_p = np.argsort(fc_p["temperature"])
@@ -161,19 +153,14 @@ def test_proper_units():
     Make sure two different proper units systems give the same answer.
     """
 
-    data_file_path = os.path.join(grackle_data_dir, "CloudyData_UVB=HM2012.h5")
-
     my_random_state = np.random.RandomState(20150725)
     for current_redshift in [0., 1., 3.]:
 
         # proper units
         chem_1 = chemistry_data()
-        chem_1.use_grackle = 1
-        chem_1.with_radiative_cooling = 0
-        chem_1.primordial_chemistry = 1
-        chem_1.metal_cooling = 1
-        chem_1.UVbackground = 1
-        chem_1.grackle_data_file = data_file_path
+        for par, val in _parameters.items():
+            setattr(chem_1, par, val)
+        metal_fraction = 0.1 * chem_1.SolarMetalFractionByMass
         chem_1.comoving_coordinates = 0
         chem_1.a_units = 1.0
         chem_1.a_value = 1.0 / (1.0 + current_redshift) / chem_1.a_units
@@ -181,7 +168,8 @@ def test_proper_units():
         chem_1.length_units = random_logscale(0, 2, random_state=my_random_state)[0]
         chem_1.time_units = random_logscale(0, 2, random_state=my_random_state)[0]
         chem_1.velocity_units = chem_1.length_units / chem_1.time_units
-        fc_1 = setup_fluid_container(chem_1, converge=False)
+        fc_1 = setup_fluid_container(chem_1, converge=False,
+                                     metal_mass_fraction=metal_fraction)
         fc_1.calculate_temperature()
         fc_1.calculate_cooling_time()
         t_sort_1 = np.argsort(fc_1["temperature"])
@@ -189,12 +177,8 @@ def test_proper_units():
 
         # proper units
         chem_2 = chemistry_data()
-        chem_2.use_grackle = 1
-        chem_2.with_radiative_cooling = 0
-        chem_2.primordial_chemistry = 1
-        chem_2.metal_cooling = 1
-        chem_2.UVbackground = 1
-        chem_2.grackle_data_file = data_file_path
+        for par, val in _parameters.items():
+            setattr(chem_2, par, val)
         chem_2.comoving_coordinates = 0
         chem_2.a_units = 1.0
         chem_2.a_value = 1.0 / (1.0 + current_redshift) / chem_2.a_units
@@ -202,7 +186,8 @@ def test_proper_units():
         chem_2.length_units = random_logscale(0, 2, random_state=my_random_state)[0]
         chem_2.time_units = random_logscale(0, 2, random_state=my_random_state)[0]
         chem_2.velocity_units = chem_2.length_units / chem_2.time_units
-        fc_2 = setup_fluid_container(chem_2, converge=False)
+        fc_2 = setup_fluid_container(chem_2, converge=False,
+                                     metal_mass_fraction=metal_fraction)
         fc_2.calculate_temperature()
         fc_2.calculate_cooling_time()
         t_sort_2 = np.argsort(fc_2["temperature"])
@@ -224,8 +209,6 @@ def test_tabulated_mmw_metal_dependence():
     mean molecular weight, when run in tabulated mode.
     """
 
-    data_file_path = os.path.join(grackle_data_dir, "CloudyData_UVB=HM2012.h5")
-
     my_random_state = np.random.RandomState(723466)
     density_units = random_logscale(-28, -26, random_state=my_random_state)[0]
     length_units = random_logscale(0, 2, random_state=my_random_state)[0]
@@ -240,12 +223,8 @@ def test_tabulated_mmw_metal_dependence():
 
         # proper units
         my_chem = chemistry_data()
-        my_chem.use_grackle = 1
-        my_chem.with_radiative_cooling = 0
-        my_chem.primordial_chemistry = 0
-        my_chem.metal_cooling = 1
-        my_chem.UVbackground = 1
-        my_chem.grackle_data_file = data_file_path
+        for par, val in _parameters.items():
+            setattr(my_chem, par, val)
         my_chem.comoving_coordinates = 0
         my_chem.a_units = 1.0
         my_chem.a_value = 1.0 / (1.0 + current_redshift) / my_chem.a_units
