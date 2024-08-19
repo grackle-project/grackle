2fa6c89576c56f1605899afeae5daa6216d1e872
Britton Smith <brittonsmith@gmail.com>
2024-08-19 13:57:15 +0100

    Update pygrackle version.

--- src/python/setup.py
+++ src/python/setup.py
@@ -108,6 +108,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     packages=find_packages(),
     setup_requires=[
