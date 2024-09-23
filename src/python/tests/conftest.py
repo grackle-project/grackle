e484a3fccc550ef6270c0bd89e0df6deae982f4a
Matthew Abruzzo <matthewabruzzo@gmail.com>
2024-09-23 19:28:31 -0400

    Adding documentation.

--- src/python/tests/conftest.py
+++ src/python/tests/conftest.py
@@ -1,18 +1,28 @@
+# this defines some basic utilities shared among all of the tests
+#
+# DO NOT import pygrackle into global scope in this file (or in any file
+# imported by this file). Some tests need to be runable without installing
+# pygrackle
+
 import os
 from typing import NamedTuple
 
 import pytest
 
-from testing_common import generate_test_results, test_answers_dir
-
 
 # this hook is used to add more command line flags to the pytest launcher
 def pytest_addoption(parser):
     parser.addoption(
+        "--answer-skip",
+        action="store_true",
+        help="Indicates that we should skip all answer tests."
+    )
+    parser.addoption(
         "--answer-store",
         action="store_true",
-        help="Indicates whether to generate test results.",
+        help="Indicates that we should generate test results.",
     )
+
 
     # in the future, I think we should revisit the default behavior when this
     # is omitted. Instead of providing a default location, I think we should
@@ -21,7 +31,7 @@ def pytest_addoption(parser):
     parser.addoption(
         "--local-dir",
         action="store",
-        default=test_answers_dir,
+        default=os.path.join(os.path.abspath(__file__), "test_answers"),
         help="Path to directory where answers are/will be stored.",
     )
 
@@ -32,13 +42,24 @@ class AnswerTestSpec(NamedTuple):
 
 
 @pytest.fixture(scope="session")
-def answertestspec(pytestconfig):
+def answertestspec(request):
     """
     Return an object specifying all user-specified directives regarding
     answer tests (whether to run them and where to store/find results)
     """
-    generate_answers = pytestconfig.getoption("--answer-store") or generate_test_results
-    answer_dir = pytestconfig.getoption("--local-dir", default=test_answers_dir)
+    if request.config.getoption("--answer-skip"):
+        if request.config.getoption("--answer-store"):
+            raise RuntimeError(
+                "It is an error to specify both --answer-skip and --answer-store"
+            )
+        pytest.skip("--answer-skip was specified")
+
+    generate_answers = (
+        request.config.getoption("--answer-store")
+        or int(os.environ.get("GENERATE_PYGRACKLE_TEST_RESULTS", 0)) == 1
+    )
+    answer_dir = request.config.getoption("--local-dir")
+
 
     if (not os.path.isdir(answer_dir)) and (not generate_answers):
         pytest.skip(f"the directory of test answers can't be found, {answer_dir}")
