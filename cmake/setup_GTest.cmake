3e7ae54efd3e6ffd1c010a509bd749370331243c
ChristopherBignamini <christopher.bignamini@gmail.com>
2024-09-27 17:55:14 +0200

    Update to GTest zip downloading instead of cloning

--- cmake/setup_GTest.cmake
+++ cmake/setup_GTest.cmake
@@ -1,29 +1,33 @@
+# This file includes logic for setting up GoogleTest
 find_package(GTest)
 
 if (NOT GTest_FOUND)
-    message(STATUS "Configure GTest from github")
+    message(STATUS
+    "Preparing to download GTest from GitHub & configure its build. Don't "
+    "worry about any mentions of Python in the following text -- that is "
+    "only used internally by the Google test build"
+    )
+
+    # NOTE: it is idiomatic to use FetchContent with a git hash rather than the
+    # name of a tag or branch (since the latter 2 can freely change). If we use
+    # the name of a tag/branch, then CMake will query GitHub on every
+    # subsequent build to check whether the tag/branch is still the same
 
     include(FetchContent)
     FetchContent_Declare(
-      googletest
-      GIT_REPOSITORY https://github.com/google/googletest.git
-      GIT_TAG        v1.13.0
+      googletest # the url contains the hash for v1.13.0
+      URL https://github.com/google/googletest/archive/b796f7d44681514f58a683a3a71ff17c94edb0c1.zip
     )
 
-    # Check if population has already been performed
-    FetchContent_GetProperties(googletest)
-    if(NOT googletest_POPULATED)
-        message(STATUS "Downloading GTest from github")
-        # Fetch the content using previously declared details
-        FetchContent_Populate(googletest)
+    # For Windows: Prevent overriding the parent project's compiler/linker settings
+    set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+    FetchContent_MakeAvailable(googletest)
 
-        # Prevent overriding the parent project's compiler/linker
-        # settings on Windows
-        set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-
-        # Bring the populated content into the build
-        add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR})
-    endif()
+elseif("${CMAKE_VERSION}" VERSION_LESS "3.20")
+    # CMake's built-in `FindGTest` module imported targets have different names
+    # in earlier CMake versions
+    add_library(GTest::gtest ALIAS GTest::GTest)
+    add_library(GTest::gtest_main ALIAS GTest::Main)
 endif()
 
 if (NOT TARGET GTest::gtest_main)
