3d5d2167376695484198d8cd0b48361748bc5cb5
Matthew Abruzzo <matthewabruzzo@gmail.com>
2025-02-03 17:58:11 -0500

    Modify answer-test args for consistency with discussion on GitHub

--- src/python/tests/conftest.py
+++ src/python/tests/conftest.py
@@ -13,27 +13,27 @@ import pytest
 # this hook is used to add more command line flags to the pytest launcher
 def pytest_addoption(parser):
     parser.addoption(
-        "--answer-skip",
-        action="store_true",
-        help="Indicates that we should skip all answer tests.",
+        "--answer-dir",
+        action="store",
+        default=None,
+        help=(
+            "Path to directory used for storing answer-tests answers. All "
+            "logic related to answer-tests is disabled unless this is "
+            "specified. When specified alonside --answer-store, logic to "
+            "generate the answers will be run (the answers are stored to this "
+            "directory). When specified WITHOUT --answer-store, the results "
+            "of each answer-test is compared against the answers that were "
+            "previously stored in this directory."
+        )
     )
+
     parser.addoption(
         "--answer-store",
         action="store_true",
-        help="Indicates that we should generate test results.",
-    )
-
-    # in the future, I think we should revisit the default behavior when this
-    # is omitted. Instead of providing a default location, I think we should
-    # just skip all answer tests (and maybe raise an error if the
-    # --answer-store flag is provided without this flag)
-    parser.addoption(
-        "--local-dir",
-        action="store",
-        default=os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), "test_answers"
+        help=(
+            "Indicates that we should store answer-test results. It is an "
+            "error to specifiy this arg without --answer-dir."
         ),
-        help="Path to directory where answers are/will be stored.",
     )
 
 
@@ -48,21 +48,20 @@ def answertestspec(request):
     Return an object specifying all user-specified directives regarding
     answer tests (whether to run them and where to store/find results)
     """
-    if request.config.getoption("--answer-skip"):
-        if request.config.getoption("--answer-store"):
-            raise RuntimeError(
-                "It is an error to specify both --answer-skip and --answer-store"
-            )
-        pytest.skip("--answer-skip was specified")
 
-    generate_answers = (
-        request.config.getoption("--answer-store")
-        or int(os.environ.get("GENERATE_PYGRACKLE_TEST_RESULTS", 0)) == 1
-    )
-    answer_dir = request.config.getoption("--local-dir")
+    generate_answers = request.config.getoption("--answer-store")
 
-    if (not os.path.isdir(answer_dir)) and (not generate_answers):
-        pytest.skip(f"the directory of test answers can't be found, {answer_dir}")
-    os.makedirs(answer_dir, exist_ok=True)
+    answer_dir = request.config.getoption("--answer-dir")
+
+    if (answer_dir is None) and generate_answers:
+        raise ValueError(
+            "--answer-store option can't be specified without --answer-dir"
+        )
+    elif answer_dir is None:
+        pytest.skip("no --answer-dir option found")
+    elif (not os.path.isdir(answer_dir)) and (not generate_answers):
+        pytest.skip(f"directory of test answers can't be found, {answer_dir}")
+    else:
+        os.makedirs(answer_dir, exist_ok=True)
 
     return AnswerTestSpec(generate_answers=generate_answers, answer_dir=answer_dir)
