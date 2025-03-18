a49bc64fd7c9f17f84cc64d888ae0cd28c77f9f5
Matthew Abruzzo <matthewabruzzo@gmail.com>
2025-03-18 08:35:46 -0400

    A few assorted tweaks
    1. A bug is fixed in the Fortran Bindings
    2. All references have been removed about the ``GRACKLE_DATA_DIR`` env
       variable. An upcoming PR (#235) is going to overhaul this machinery,
       but it probably won't be merged the 3.4 release

--- src/python/pygrackle/utilities/data_path.py
+++ src/python/pygrackle/utilities/data_path.py
@@ -16,20 +16,11 @@ import os
 from pygrackle.__config__ import _is_editable_installation
 from pygrackle.utilities.misc import dirname
 
-grackle_data_dir = os.environ.get("GRACKLE_DATA_DIR")
-if (grackle_data_dir is None) and _is_editable_installation():
+if _is_editable_installation():
     # Note, this only works with an editable install of pygrackle.
     _install_dir = dirname(os.path.abspath(__file__), level=5)
     grackle_data_dir = os.path.join(_install_dir, "input")
-elif (grackle_data_dir is None):
+else (grackle_data_dir is None):
     raise RuntimeError(
         "in non-editable pygrackle installations, like this one, "
-        f"grackle_data_dir can only be imported from {__file__} if it is set "
-        "by the GRACKLE_DATA_DIR environment variable"
-    )
-
-if not os.path.isdir(grackle_data_dir):
-    raise RuntimeError(
-        f"grackle_data_dir not set to a valid directory: {grackle_data_dir}. "
-        "Use the GRACKLE_DATA_DIR environment variable to set path to "
-        "Grackle data.")
+        f"grackle_data_dir cannot be imported from {__file__}."
