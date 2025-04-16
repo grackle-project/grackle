284cb5852c0a9907952385a053a93473544ebfb6
ChristopherBignamini <christopher.bignamini@gmail.com>
2025-04-16 12:53:41 +0200

    Reindexing (i/j/k-1)

---
+++ src/clib/scale_fields_g-cpp.h
@@ -0,0 +1,28 @@
+// See LICENSE file for license and copyright information
+
+/// @file scale_fields_g-cpp.h
+/// @brief Declares signature of scale_fields_g
+
+// This file was initially generated automatically during conversion of the
+// scale_fields_g function from FORTRAN to C++
+
+#ifndef SCALE_FIELDS_G_CPP_H
+#define SCALE_FIELDS_G_CPP_H
+
+#include "grackle.h"             // gr_float
+#include "fortran_func_decls.h"  // gr_mask_int
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+void scale_fields_g(
+  int* imetal, gr_float* factor, chemistry_data* my_chemistry,
+  grackle_field_data* my_fields
+);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif /* __cplusplus */
+
+#endif /* SCALE_FIELDS_G_CPP_H */
