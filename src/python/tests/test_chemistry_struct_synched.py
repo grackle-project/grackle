c27c0f208ba383745330e7c958cbb666298999f5
Matthew Abruzzo <matthewabruzzo@gmail.com>
2025-04-17 09:54:26 -0400

    Move CastXML parsing to a separate script
    This change was made in anticipation of shifting the
    "test_grackle_chemistry_field_synched" test from the pygrackle
    test-suite to the core-lib test suite.
    
    In more detail, the test doesn't really belong in the pygrackle
    test-suite since it is looking at header files. As currently written,
    the test current places a strong constraint on the way in which we
    organize our public headers (it currently can't read a header that
    depends on an autogenerated header file). Shifting this test to the
    core-lib test suite will remove this limitation

--- src/python/tests/test_chemistry_struct_synched.py
+++ src/python/tests/test_chemistry_struct_synched.py
@@ -19,49 +19,13 @@ import os.path
 import shutil
 import subprocess
 import tempfile
-import warnings
-import xml.etree.ElementTree # may not be the optimal xml parser
 
 import pytest
 
 from pygrackle.grackle_wrapper import _wrapped_c_chemistry_data
+from testing_common import grackle_install_dir
 
 _CASTXML_INSTALLED = shutil.which('castxml') is not None
-
-def _find_element_by_ids(id_str, root, expect_single = False):
-    # id_str provides a list of one or more space separated ids
-    id_set = set(id_str.split(' '))
-    assert len(id_set) or (len(id_set) != 0 and not expect_single)
-
-    # implicit assumption: every element has a unique id
-    out = [e for e in root if e.attrib['id'] in id_set]
-    return out
-
-def _field_type_props(elem, root):
-    assert elem.tag == 'Field'
-    ptr_level = 0
-    while True:
-        elem = _find_element_by_ids(elem.attrib['type'], root, True)[0]
-        if elem.tag == 'PointerType':
-            ptr_level += 1
-        elif elem.tag == 'CvQualifiedType':
-            pass # just ignore this
-        else:
-            return elem.attrib['name'] + (ptr_level * "*")
-
-def _unwrap_struct_from_typedef(elem, root):
-    # this is called when elem corresponds to a type declared with typedef
-    # -> this function returns the xml-object representing the underlying struct
-    #    that is aliased by the typedef
-    _tmp = _find_element_by_ids(elem.attrib['type'], root,
-                                expect_single = True)[0]
-    if _tmp.tag == 'ElaboratedType':
-        struct_elem = _find_element_by_ids(_tmp.attrib['type'], root,
-                                           expect_single = True)[0]
-    else:
-        struct_elem = _tmp
-    assert struct_elem.tag == 'Struct'  # <-- sanity check!
-    return struct_elem
 
 def query_struct_fields(struct_name, path):
     """
@@ -106,77 +70,21 @@ def query_struct_fields(struct_name, path):
     # Step 3: run the command
     subprocess.run(command, check=True)
 
-    if not os.path.isfile(xml_fname):
-        raise RuntimeError(
-            f"something went wrong while executing {' '.join(command)}"
-        )
-
-    # Step 4: parse the output and then delete the temporary file
-    tree = xml.etree.ElementTree.parse(xml_fname)
-    os.remove(xml_fname)
-
-    # in version 1.1.0 of gccxml format, there is a root node whose tag is
-    # CastXML & lists the version attribute
-    root = tree.getroot()
-    assert root.tag == 'CastXML'
-    if root.attrib['format'] not in ['1.1.0', '1.1.5']:
-        warnings.warn(
-            "Code was only tested against CastXML format versions 1.1.0 & "
-            "1.1.5. The file produced by CastXML uses version "
-            f"{root.attrib['format']}"
-        )
-
-    # under the root node, the children are listed in a flat structure can be
-    # - typedef declarations, struct/class declarations, namespace declarations,
-    #   Field declarations, class member declarations, global variable
-    #   declarations
-    # - it also has entries for describing other types
-
-    # Step 5: now extract the necessary information
-    # Step 5a: find the element with name attribute that matches struct_name
-    matches = (
-        tree.getroot().findall(".//Struct[@name='" + struct_name + "']") +
-        tree.getroot().findall(".//Typedef[@name='" + struct_name + "']")
-    )
-
-    if len(matches) == 0:
-        raise RuntimeError(f"no struct name '{struct_name}' was found")
-    elif len(matches) > 2:
-        raise RuntimeError(f"found more than 2 matches for '{struct_name}'")
-    elif (len(matches) == 1) and (matches[0].tag == 'Struct'):
-        # in this case, we defined a struct named {struct_name} and didn't
-        # use typedef to declare any type aliases of the struct
-        # -> in other words, we can only refer to the type in our C code as
-        #    `struct {struct_name}`
-        struct_elem = matches[0]
-    elif (len(matches) == 1) and (matches[0].tag == 'Typedef'):
-        # in this case, we used typedef to define a type called {struct_name}
-        # that aliases an "anonymous struct"
-        # -> in other words, we can only refer to the type in our C code as
-        #    `{struct_name}`
-        struct_elem = _unwrap_struct_from_typedef(matches[0], root)
-    elif ((len(matches) == 2) and (matches[0].tag == 'Struct') and
-          (matches[1].tag == 'Typedef')):
-        # in this case we probably defined a struct called {struct_name} and
-        # used typedef to declare an alias type called {struct_name}
-        # -> in other words, the types `struct {struct_name}` and
-        #    `{struct_name}` refer to the same type
-        struct_elem = matches[0]
-        # perform a sanity check to confirm that typedef indeed aliases the
-        # corresponding struct
-        # -> note: I'm not entirely sure this is always guaranteed to work
-        assert struct_elem == _unwrap_struct_from_typedef(matches[1], root)
-    else:
-        raise RuntimeError("SOMETHING WENT WRONG")
-
-    # Step 5b, find the child elements of the struct
+    # Step 4: run script to analyze the outputs
+    command = [
+        f"{grackle_install_dir}/tests/scripts/castxml_output_reader.py",
+        "--input", xml_fname,
+        "--type", struct_name
+    ]
+    rslt = subprocess.run(command, capture_output=True, check=True, encoding="utf8")
     out = []
-    for member in _find_element_by_ids(struct_elem.attrib['members'], root,
-                                       expect_single = False):
-        if member.tag == 'Field': # ignore auto-generated class methods
-            out.append(
-                (_field_type_props(member, root), member.attrib['name'])
-            )
+    for line in rslt.stdout.splitlines():
+        if len(line) == "":
+            continue
+        pairs = line.split(",")
+        assert len(pairs) == 2
+        name, dtype = pairs
+        out.append((name.strip(), dtype.strip()))
     return out
 
 @pytest.mark.skipif(not _CASTXML_INSTALLED,
@@ -192,7 +100,7 @@ def test_grackle_chemistry_field_synched():
 
     # now, categorize the fields by their datatype
     field_sets = {'char*' : set(), 'int' : set(), 'double' : set()}
-    for dtype, name in member_list:
+    for name, dtype in member_list:
         field_sets[dtype].add(name)
 
     # finally lets compare
