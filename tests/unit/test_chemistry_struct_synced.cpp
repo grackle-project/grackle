a0054ffa45cf0e7fa7a513e11cfe9e18c44a60c4
Matthew Abruzzo <matthewabruzzo@gmail.com>
2025-05-18 12:02:00 -0400

    an improvement in test_chemistry_struct_synced

--- tests/unit/test_chemistry_struct_synced.cpp
+++ tests/unit/test_chemistry_struct_synced.cpp
@@ -15,34 +15,29 @@
 using MemberTypeNameMap = std::map<std::string, std::string>;
 
 #if defined(stringify) || defined(stringify_helper)
-#error "stringify or stringify_helper was already defined"
-#elif !defined(GR_SRC_ROOT_PATH)
-#error "GR_SRC_ROOT_PATH macro isn't defined"
-#elif !defined(IFLAG_DIR_GENERATED)
+  #error "stringify or stringify_helper was already defined"
+#elif !defined(READER_PATH) || !defined(XML_PATH)
+  #error "either the READER_PATH or XML_PATH macro isn't defined"
 #else
-#define stringify(s) stringify_helper(s)
-#define stringify_helper(s) #s
-
-static const char* GLOBAL_grackle_src_root_path = stringify(GR_SRC_ROOT_PATH);
-static const char* GLOBAL_iflag_0 = "-I" stringify(IFLAG_DIR_GENERATED);
+  #define stringify(s) stringify_helper(s)
+  #define stringify_helper(s) #s
+  static const char* GLOBAL_reader_path = stringify(READER_PATH);
+  static const char* GLOBAL_xml_path = stringify(XML_PATH);
 #endif
 
-bool program_exists(std::string program) {
-  grtest::Command cmd;
-  cmd.program = "command";
-  cmd.args = {"-v", program};
-  cmd.stdout_path = "/dev/null";
-  cmd.stderr_path = "/dev/null";
 
-  return grtest::process_output(cmd).status == 0;
-}
+struct MemberTypeNameMapResult {
+  std::string err;
+  MemberTypeNameMap map;
+};
+
 
 /// try to parse a string that holds a file where each line has the format
 /// "<member_name>, <member_type>" and create a MemberTypeNameMap
 ///
 /// @note
 /// although std::regex is known to be **VERY** slow, it is convenient
-static std::optional<MemberTypeNameMap> try_parse_(const std::string& s) {
+static MemberTypeNameMapResult try_parse_(const std::string& s) {
   MemberTypeNameMap mapping;
   std::regex myregex("^([^,]+), *([^,]+) *$");
   std::size_t len = s.size();
@@ -58,92 +53,67 @@ static std::optional<MemberTypeNameMap> try_parse_(const std::string& s) {
       if (member_type == "char*") { member_type = "string"; }
       mapping[member_name] = member_type;
     } else {
-      std::fprintf(stderr, "encountered parsing problem!\n");
-      return {};
+      MemberTypeNameMapResult out;
+      out.err = "problem converting the reader's output to a map!";
+      return out;
     }
     pos = lineend+1;
   }
-  return {mapping};
+
+  return MemberTypeNameMapResult{"", mapping};
 }
 
-/// Query the data-members of a struct. Returns a std::map where the keys are
-/// the names of types and the values are the set of all member-names that have
-/// that type. The result of this function is used for checking that efforts for
-/// "manual reflection" of a struct's members are correct
+/// Reads the previously generated xml-file (produced by the build-system with
+/// the castxml tool) to determine the data-members of a struct. Returns a
+/// std::map that maps the names of data-members to their type. The result of
+/// this function is used for checking the correctness of functionallity that
+/// effectively implements "manual reflection" of the struct's members.
 ///
-/// @returns an empty option to denote **any** errors
+/// @returns a non-empty error-message to denote **any** errors
 ///
-/// In more detail, this function has 3 phases:
-/// 1. castxml is used to parse a c++ header file (or compatible C header file)
-///    & spit out an xml file that summarizes all of the contained declarations
+/// In more detail, this function has 2 phases:
+/// 1. we check if a valid file was written by build-system
 /// 2. a python script is invoked to extract relevant information from the xml
 ///    file (it uses python's builtin xml-parsing library)
 /// 3. we read in the extracted information and put it into the output format
 ///
 /// @note
-/// There are some issues with running this test if the source directory is
-/// modified after this file is compile. In the future, a compelling case could
-/// be made that the build-system should directly invoke castxml. For now, this
-/// is a significant improvement over the python tests we are replacing.
-///
-/// @note
-/// In the future, we'll be able to replace this library with a simplified
-/// alternative. For example, we could use 1 of the following lightweight
-/// librarie to directly infer members of aggregates (this includes C structs,
-/// https://en.cppreference.com/w/cpp/language/aggregate_initialization) at
-/// compile-time (using compiler-extensions)
-/// - https://github.com/boostorg/pfr  (this doesn't need the rest of boost)
-/// - https://github.com/stephenberry/glaze
-/// Alteratively, C++26 is on track to make "reflection" of fields a standard
-/// C++ feature.
-std::optional<MemberTypeNameMap> query_struct_members(
-  std::string struct_name, std::string path_relative_to_src_root
-)
+/// In the future, we'll be able to replace this with a simplified alternative:
+/// - For example, we could use 1 of the following lightweight librarie to
+///   directly infer members of aggregates (this includes C structs,
+///   https://en.cppreference.com/w/cpp/language/aggregate_initialization) at
+///   compile-time (using compiler-extensions)
+///   - https://github.com/boostorg/pfr  (this doesn't need the rest of boost)
+///   - https://github.com/stephenberry/glaze
+/// - Alteratively, C++26 is on track to make "reflection" of fields a standard
+///   C++ feature.
+MemberTypeNameMapResult query_struct_members(std::string struct_name)
 {
   // step 0: basic setup
   // ===================
-  std::string src_root = GLOBAL_grackle_src_root_path + std::string("/");
-  // -> get path to header file
-  std::string hdr_path = src_root + path_relative_to_src_root;
-  // -> get path to xml-reader
-  std::string xml_reader = src_root + "tests/scripts/castxml_output_reader.py";
-  // -> make a temporary directory (cleaned up when leaving function)
-  grtest::ScratchDir temp_dir;
-  // -> determine path to intermediate xml file
-  std::string xml_path = temp_dir.get_path() + "/castxml-output.xml";
+  // -> this is the path to file created by the build-system
+  std::string xml_path = GLOBAL_xml_path;
+  // -> this is the path to the executable script to parse the file
+  std::string xml_reader = GLOBAL_reader_path;
 
-  // step 1: run castxml
-  // ===================
-  grtest::Command castxml_cmd;
-  castxml_cmd.program = "castxml";
-  castxml_cmd.args = {
-    // tell castxml's compiler to preprocess/compile but don't link
-    "-c",
-    // tell castxml about IFLAGs
-    GLOBAL_iflag_0,
-    // tell castxml's compiler that the language is c++
-    "-x", "c++",
-    // the next required option configure castxml's internal Clang compiler.
-    // The second part needs to specify an installed compiler (we may need
-    // to support configuration of that path)
-    "--castxml-cc-gnu", "g++",
-    // specify the output xml format version
-    "--castxml-output=1",
-    // specify the output location
-    "-o", xml_path,
-    // specify the file to process
-    hdr_path
-  };
-  castxml_cmd.stdout_path = ""; // <- capture stdout
-  castxml_cmd.stderr_path = ""; // <- capture stderr
-  grtest::ProcessOutput castxml_rslt = grtest::process_output(castxml_cmd);
-  if ((castxml_rslt.status != 0) || (castxml_rslt.stdout_str.size() != 0)) {
-    std::string tmp = grtest::summarize_cmd_and_rslt(castxml_cmd, castxml_rslt);
-    std::fprintf(stderr, "unexpected outcome:\n%s\n", tmp.c_str());
-    return {};
-  } else if (castxml_rslt.stderr_str.size() != 0) {
-    std::fprintf(stderr, "%s\n", castxml_rslt.stderr_str.c_str());
+  // step 1: check that xml exists
+  // =============================
+  std::FILE* f = std::fopen(xml_path.c_str(), "r");
+  if (f == NULL) {
+    return MemberTypeNameMapResult{
+      "The build-system failed to create the xml-file. (THIS SHOULDN'T HAPPEN)",
+      {}
+    };
+  } else if (std::fgetc(f) == EOF) {
+    std::string msg(
+      "The build-system wrote an empty xml-file, which usually means that "
+      "castxml wasn't installed. If you install castxml, you need to instruct "
+      "CMake to rebuild of the target containing this test (to trigger a rebuild "
+      "the XML file). It's usually easiest to clean things up and build again."
+    );
+    return MemberTypeNameMapResult{msg, {}};
   }
+  std::fclose(f);
 
   // step 2: parse the xml
   // =====================
@@ -156,8 +126,8 @@ std::optional<MemberTypeNameMap> query_struct_members(
   if (xmlread_rslt.status != 0) {
     std::string tmp = grtest::summarize_cmd_and_rslt(xmlread_cmd, xmlread_rslt);
     std::fprintf(stderr, "unexpected outcome:\n%s\n", tmp.c_str());
-    return {};
-  } else if (castxml_rslt.stderr_str.size() != 0) {
+    return MemberTypeNameMapResult{"Something weird happened", {}};
+  } else if (xmlread_rslt.stderr_str.size() != 0) {
     std::fprintf(stderr, "%s\n", xmlread_rslt.stderr_str.c_str());
   }
 
@@ -203,19 +173,15 @@ testing::AssertionResult HasExpectedMembers (
 }
 
 
-#define EXPECT_MEMBERS(member_map, struct_name, path_relative_to_srcroot)     \
-  if (! program_exists("castxml") ) {                                         \
-    GTEST_SKIP() << "the castxml tool is required to run this test";          \
+#define EXPECT_MEMBERS(member_map, struct_name)                               \
+  /* use determine the actual members */                                      \
+  MemberTypeNameMapResult ref_TEMP_ = query_struct_members( struct_name );    \
+  if (ref_TEMP_.err.size() > 0 ) {                                            \
+    GTEST_SKIP() << "Issue inferring members of " << struct_name << ". "      \
+                 << ref_TEMP_.err;                                            \
   }                                                                           \
                                                                               \
-  /* use castxml to determine the actual members */                           \
-  std::optional<MemberTypeNameMap> ref_map_TEMP_ =                            \
-    query_struct_members( struct_name, path_relative_to_srcroot );            \
-  if (! bool(ref_map_TEMP_) ) {                                               \
-    GTEST_SKIP() << "issue inferring the members of " << struct_name;         \
-  }                                                                           \
-                                                                              \
-  GTEST_EXPECT_TRUE(HasExpectedMembers(struct_name, member_map, *ref_map_TEMP_))
+  GTEST_EXPECT_TRUE(HasExpectedMembers(struct_name, member_map, ref_TEMP_.map))
 
 /// this is used down below!
 extern "C" { typedef const char* ParamNameFn(unsigned int); }
@@ -237,5 +203,5 @@ TEST(ChemistryData, SynchronizedMembers) {
   }
 
   // here we check
-  EXPECT_MEMBERS(member_map, "chemistry_data", "src/include/grackle.h");
+  EXPECT_MEMBERS(member_map, "chemistry_data");
 }
