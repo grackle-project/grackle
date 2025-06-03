30598a4a0cf33f69172561dcf72c111ff2e93dd6
Matthew Abruzzo <matthewabruzzo@gmail.com>
2025-06-03 10:56:01 -0400

    Add check_packaged_dependency_licenses.py
    This verifies that all libraries included in the wheel have a license entry
    and confirm that we didn't include any unnecessary licenses
    
    This is invoked as part of building the binary wheels

---
+++ scripts/wheels/check_packaged_dependency_licenses.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+"""
+Check that the vendored dependencies & binary licenses all match
+
+This is intended to be installed once the pygrackle wheel has been installed
+"""
+
+from dataclasses import dataclass
+import fnmatch
+import re
+import os
+import pathlib
+import platform
+from typing import Optional
+import sys
+
+
+@dataclass
+class LicenseInfo:
+    name: str
+    location: str  # location where the info was parsed from
+    availability: Optional[str] = None
+    description: Optional[str] = None
+    files: Optional[str] = None # specifies file(s) that the license applies to
+
+
+def parse_license_file(path):
+
+    def _chunks(f):
+        chunk = None
+        for lineno, line in enumerate(f, start=1):
+            m = re.match(r"^(?P<field>[A-Za-z]+):\s*(?P<value>\S(.*\S)?)$", line)
+            if m is None:
+                yield chunk
+                chunk = None
+            elif m.group("field").lower() == "name":
+                yield chunk
+                chunk = LicenseInfo(name=m.group("value"), location=f"{path!s}:{lineno}")
+            elif m is not None:
+                setattr(chunk, m.group("field").lower(), m.group("value"))
+        yield chunk
+
+    out = {}
+    with open(path, "r") as f:
+        return [chunk for chunk in _chunks(f) if chunk is not None]
+
+
+def get_vendored_libs(pygrackle_dir):
+    if platform.system() == "Darwin":
+        lib_dir = pygrackle_dir / ".dylibs"
+    else:
+        lib_dir = pygrackle_dir.parent / "pygrackle.libs"
+    lib_paths = []
+    with os.scandir(lib_dir) as it:
+        for entry in it:
+            if entry.is_file():
+                lib_paths.append(entry.path)
+            else:
+                raise RuntimeError(f"{lib_dir} holds a non-file: {entry.path}")
+    return set(lib_paths)
+
+
+def main():
+    try:
+        import pygrackle
+    except ImportError:
+        print("ERROR: this check requires pygrackle to be installed")
+        return 1
+    pygrackle_dir = pathlib.Path(pygrackle.__file__).parent
+
+    # get a list of external libraries that we have packaged
+    lib_paths = get_vendored_libs(pygrackle_dir)
+
+    # find and parse the license file
+    distinfo = list(pygrackle_dir.parent.glob("pygrackle-*.dist-info"))[0]
+    license_entries = parse_license_file(distinfo / "licenses" / "LICENSE")
+
+    # now, we will go through and make sure every license_entry corresponds to 1 or more
+    # lib_path entries (and vice-versa)
+    all_matches = set()
+    for entry in license_entries:
+        patterns = entry.files.split()  # patterns may be separated by whitespace
+        for pat in patterns:
+            matches = fnmatch.filter(lib_paths, str(pygrackle_dir.parent / pat))
+            if len(matches) == 0:
+                print(
+                    f"ERROR: the file pattern `{pat}` describe a packaged library. This "
+                    f"pattern describes `{entry.location}`, from `{entry.location}`"
+                )
+                return 1
+            all_matches.update(matches)
+    if len(all_matches) != len(lib_paths):
+        no_matches = lib_paths.difference(all_matches)
+        tmp = "ERROR: packaged libs don't have license entries"
+        print(tmp, *no_matches, sep="\n   ")
+        return 1
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
