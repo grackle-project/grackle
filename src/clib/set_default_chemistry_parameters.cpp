4e87946c38274696d9f03912367585f9b258cf26
Matthew Abruzzo <matthewabruzzo@gmail.com>
2025-09-30 10:40:20 -0400

    convert set_default_chemistry_parameters.c to C++

--- src/clib/set_default_chemistry_parameters.cpp
+++ src/clib/set_default_chemistry_parameters.cpp
@@ -11,48 +11,45 @@
 / software.
 ************************************************************************/
 
-#include <string.h>
-#include <stdio.h>
-#include <math.h>
-
+#include <cstdio>
+#include "grackle.h"
 #include "grackle_macros.h"
-#include "grackle_types.h"
-#include "grackle_chemistry_data.h"
 
+// set the global variables
 int grackle_verbose = FALSE;
 
-chemistry_data *grackle_data = NULL;
+chemistry_data *grackle_data = nullptr;
 chemistry_data_storage grackle_rates;
 
-int local_initialize_chemistry_parameters(chemistry_data *my_chemistry)
+extern "C" int local_initialize_chemistry_parameters(chemistry_data *my_chemistry)
 {
-  if (my_chemistry == NULL){
-    return FAIL;
+  if (my_chemistry == nullptr){
+    return GR_FAIL;
   }
   // assign the default value to each field of my_chemistry
   #define ENTRY(FIELD, TYPE, DEFAULT_VAL) my_chemistry->FIELD = DEFAULT_VAL;
   #include "grackle_chemistry_data_fields.def"
   #undef ENTRY
-  return SUCCESS;
+  return GR_SUCCESS;
 }
 
-int set_default_chemistry_parameters(chemistry_data *my_grackle)
+extern "C" int set_default_chemistry_parameters(chemistry_data *my_grackle)
 {
   grackle_data = my_grackle;
   return local_initialize_chemistry_parameters(my_grackle);
 }
 
-int gr_initialize_field_data(grackle_field_data *my_fields)
+extern "C" int gr_initialize_field_data(grackle_field_data *my_fields)
 {
-  if (my_fields == NULL) {
-    fprintf(stderr, "gr_initial_field_data was passed a NULL pointer\n");
-    return FAIL;
+  if (my_fields == nullptr) {
+    std::fprintf(stderr, "gr_initial_field_data was passed a NULL pointer\n");
+    return GR_FAIL;
   }
 
   my_fields->grid_rank = -1;
-  my_fields->grid_dimension = NULL;
-  my_fields->grid_start = NULL;
-  my_fields->grid_end = NULL;
+  my_fields->grid_dimension = nullptr;
+  my_fields->grid_start = nullptr;
+  my_fields->grid_end = nullptr;
   my_fields->grid_dx = -1.0;
 
   // below, we use X-Macros to modify all members of my_fields, which are used
@@ -68,5 +65,5 @@ int gr_initialize_field_data(grackle_field_data *my_fields)
   #include "field_data_misc_fdatamembers.def"
   #undef ENTRY
 
-  return SUCCESS;
+  return GR_SUCCESS;
 }
