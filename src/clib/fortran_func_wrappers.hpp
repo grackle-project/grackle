c1255856308600adea4916256b3819497b831044
ChristopherBignamini <christopher.bignamini@gmail.com>
2025-09-30 15:50:20 +0200

    Commented wrapper removed

--- src/clib/fortran_func_wrappers.hpp
+++ src/clib/fortran_func_wrappers.hpp
@@ -703,35 +703,6 @@ inline void rate_timestep_g(
                               );
 }
 
-
-// inline void scale_fields_g(
-//   int imetal, gr_float factor, chemistry_data* my_chemistry,
-//   grackle_field_data* my_fields
-// ) {
-
-//   FORTRAN_NAME(scale_fields_g)(
-//       &my_chemistry->primordial_chemistry, &imetal, &my_chemistry->use_dust_density_field, &my_chemistry->metal_chemistry,
-//       &my_chemistry->metal_abundances, &my_chemistry->dust_species, &my_chemistry->multi_metals, &my_chemistry->grain_growth, &my_chemistry->dust_sublimation, &factor,
-//       &my_fields->grid_start[0], &my_fields->grid_end[0], &my_fields->grid_start[1], &my_fields->grid_end[1], &my_fields->grid_start[2], &my_fields->grid_end[2], &my_fields->grid_dimension[0], &my_fields->grid_dimension[1], &my_fields->grid_dimension[2],
-//       my_fields->density, my_fields->e_density, my_fields->HI_density, my_fields->HII_density, my_fields->HeI_density, my_fields->HeII_density, my_fields->HeIII_density,
-//       my_fields->HM_density, my_fields->H2I_density, my_fields->H2II_density, my_fields->DI_density, my_fields->DII_density, my_fields->HDI_density, my_fields->DM_density, my_fields->HDII_density, my_fields->HeHII_density,
-//       my_fields->metal_density, my_fields->dust_density,
-//       my_fields->CI_density, my_fields->CII_density, my_fields->CO_density, my_fields->CO2_density,
-//       my_fields->OI_density, my_fields->OH_density, my_fields->H2O_density, my_fields->O2_density,
-//       my_fields->SiI_density, my_fields->SiOI_density, my_fields->SiO2I_density,
-//       my_fields->CH_density, my_fields->CH2_density, my_fields->COII_density, my_fields->OII_density,
-//       my_fields->OHII_density, my_fields->H2OII_density, my_fields->H3OII_density, my_fields->O2II_density,
-//       my_fields->Mg_density, my_fields->Al_density, my_fields->S_density, my_fields->Fe_density,
-//       my_fields->SiM_dust_density, my_fields->FeM_dust_density, my_fields->Mg2SiO4_dust_density, my_fields->MgSiO3_dust_density, my_fields->Fe3O4_dust_density,
-//       my_fields->AC_dust_density, my_fields->SiO2_dust_density, my_fields->MgO_dust_density, my_fields->FeS_dust_density, my_fields->Al2O3_dust_density,
-//       my_fields->ref_org_dust_density, my_fields->vol_org_dust_density, my_fields->H2O_ice_dust_density,
-//       my_fields->local_ISM_metal_density, my_fields->ccsn13_metal_density, my_fields->ccsn20_metal_density, my_fields->ccsn25_metal_density, my_fields->ccsn30_metal_density,
-//       my_fields->fsn13_metal_density, my_fields->fsn15_metal_density, my_fields->fsn50_metal_density, my_fields->fsn80_metal_density,
-//       my_fields->pisn170_metal_density, my_fields->pisn200_metal_density, my_fields->y19_metal_density
-//     );
-
-// }
-
 /// Uses one linearly implicit Gauss-Seidel sweep of a backward-Euler time
 /// integrator to advance the rate equations by one (sub-)cycle (dtit).
 inline void step_rate_g(
