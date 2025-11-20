073446c9f5e99da2de33e5bc4c9107a27a97d3d0
ChristopherBignamini <christopher.bignamini@gmail.com>
2025-11-20 19:15:57 +0100

    Fortran wrapper and declaration removed

--- src/clib/fortran_func_decls.h
+++ src/clib/fortran_func_decls.h
@@ -139,16 +139,6 @@ void FORTRAN_NAME(cool1d_cloudy_g)(
   double* clHeating, gr_mask_type* itmask
 );
 
-void FORTRAN_NAME(cool1d_cloudy_old_tables_g)(
-  gr_float* d_data_ptr, gr_float* de_data_ptr, double* rhoH,
-  double* metallicity, int* in, int* jn, int* kn, int* is, int* ie, int* j,
-  int* k, double* logtem, double* edot, double* comp2, int* ispecies,
-  double* dom, double* zr, int* icmbTfloor, int* iClHeat, double* clEleFra,
-  long long* clGridRank, long long* clGridDim, double* clPar1, double* clPar2,
-  double* clPar3, double* clPar4, double* clPar5, long long* clDataSize,
-  double* clCooling, double* clHeating, gr_mask_type* itmask
-);
-
 void FORTRAN_NAME(gaussj_g)(
   int* n, double* a_data_ptr, double* b, int* ierr
 );
