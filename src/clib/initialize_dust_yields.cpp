ffe87a5bc5ed1774fc4f6c8b6f3eb5402cb2e481
Matthew Abruzzo <matthewabruzzo@gmail.com>
2025-12-05 16:22:01 -0500

    move some machinery out of initialize_dust_yields.cpp

--- src/clib/initialize_dust_yields.cpp
+++ src/clib/initialize_dust_yields.cpp
@@ -18,95 +18,10 @@
 #include "grackle_macros.h"
 #include "grackle_chemistry_data.h"
 #include "initialize_dust_yields.hpp" // forward declarations
+#include "inject_model/raw_data.hpp"
 #include "LUT.hpp"
 #include "opaque_storage.hpp"
 #include "status_reporting.h" // GrPrintAndReturnErr
-
-/// datastructures declared in this namespace are a temporarily introduced for
-/// encoding the injection model data, until we shift to using HDF5
-///
-/// @note
-/// At this point, the main barrier to adopting HDF5 is that I would like to
-/// first remove all usage of MetalNuclideYieldProps::total_yield (so that we
-/// don't need to encode it in the HDF5 file).
-namespace grackle::impl::inj_model_input {
-
-/// the number of opacity-related coefficients that are grouped together
-/// in the opacity table
-static constexpr int N_Opacity_Coef = 4;
-
-/// the number of Tdust values in an opacity table for a given grain species
-/// and injection pathway. For each Tdust, there are N_Opacity_Coef coefficients
-static constexpr int N_Tdust_Opacity_Table = 35;
-
-struct MetalNuclideYieldProps;
-struct GrainSpeciesYieldProps;
-
-/// This is used to organize the storage of the injection pathway data
-///
-/// Essentially, this dictates how the data is organized on disk (as part of
-/// the Grackle library). This data will be somewhat reordered to actually
-/// perform calculations
-///
-/// There are a few reasons why it makes some sense to store this data in a
-/// a slightly different format than the way is organized during the core
-/// calculations:
-/// - the way we organize it here (which has some similarities to a struct of
-///   arrays) is a lot less error-prone.
-/// - the way we organize it here allows us to use a more sparse representation
-///   (i.e. we omit yields from pathways when the yields are 0)
-///
-/// @note
-/// The plan is to eventually shift the data into a file loaded at runtime
-struct InjectionPathwayInputData {
-  const char* name;
-
-  const MetalNuclideYieldProps* metal_nuclide_yields;
-  int n_metal_nuclide_yields;
-
-  const GrainSpeciesYieldProps* initial_grain_props;
-  int n_injected_grain_species;
-};
-
-
-struct MetalNuclideYieldProps {
-  /// name of the nuclide
-  const char* name;
-
-  /// fraction of non-primordial injection corresponding to the nuclide in
-  /// the gas phase
-  double gas_yield;
-
-  /// total fraction of non-primordial injection corresponding to the nuclide
-  /// - I'm pretty sure we can get rid of this information in the future
-  double total_yield;
-};
-
-/// Each injection array will hold an array of these structs
-struct GrainSpeciesYieldProps {
-  /// name of the Grain Species
-  const char* name;
-
-  /// Initial yield
-  double nonprimoridal_yield_frac;
-
-  /// the 1st, 2nd, and 3rd order moments of the initial size distribution for
-  /// the grain species.
-  ///
-  /// - Element 0 specifies \f$\langle r^1 \rangle_j\f$. This is the grain
-  ///   species's average **INITIAL** radius. Has units of cm.
-  /// - Element 1 specifies \f$\langle r^2 \rangle_j\f$. The product of this
-  ///   value and π gives the grain species's average **INITIAL**
-  ///   cross-section. Has units of centimeters squared.
-  /// - Element 2 specifies \f$\langle r^3 \rangle_j\f$. The product of this
-  ///   value and (4π/3) is the grain species's average **INITIAL** volume.
-  ///   Has units of centimeters cubed.
-  double size_moments[3];
-
-  double opacity_coef_table[N_Tdust_Opacity_Table][N_Opacity_Coef];
-};
-
-} // namespace grackle::impl::inj_model_input
 
 namespace {  // stuff inside an anonymous namespace is local to this file
 
