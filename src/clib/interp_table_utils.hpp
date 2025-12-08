d7727b33a6a666b3f68f297f915c64c7b15eb835
Matthew Abruzzo <matthewabruzzo@gmail.com>
2025-12-08 12:18:56 -0500

    apply clang-format to src/clib/interp_table_utils.hpp

--- src/clib/interp_table_utils.hpp
+++ src/clib/interp_table_utils.hpp
@@ -13,15 +13,14 @@
 #ifndef INTERP_TABLE_UTILS_HPP
 #define INTERP_TABLE_UTILS_HPP
 
-#include "grackle.h" // gr_interp_grid, GRACKLE_CLOUDY_TABLE_MAX_DIMENSION
-#include "grackle_macros.h" // GRACKLE_FREE
+#include "grackle.h"  // gr_interp_grid, GRACKLE_CLOUDY_TABLE_MAX_DIMENSION
+#include "grackle_macros.h"  // GRACKLE_FREE
 
 namespace grackle::impl {
 
 /// Free memory associated with a #gr_interp_grid_props instance
 inline void free_interp_grid_props_(gr_interp_grid_props* props,
-                                    bool use_delete)
-{
+                                    bool use_delete) {
   for (int i = 0; i < GRACKLE_CLOUDY_TABLE_MAX_DIMENSION; i++) {
     if (use_delete) {
       if (props->parameters[i] != nullptr) {
@@ -35,8 +34,7 @@ inline void free_interp_grid_props_(gr_interp_grid_props* props,
 }
 
 /// Free memory associated with a #gr_interp_grid
-inline void free_interp_grid_(gr_interp_grid* grid)
-{
+inline void free_interp_grid_(gr_interp_grid* grid) {
   free_interp_grid_props_(&(grid->props), /* use_delete = */ false);
   GRACKLE_FREE(grid->data);
 }
