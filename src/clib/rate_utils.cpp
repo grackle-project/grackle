ff9eca6b864d15fdd1b449bff0d75db29336e85a
Matthew Abruzzo <matthewabruzzo@gmail.com>
2025-12-11 20:58:36 -0500

    rename rateprop -> ratequery::Descr (as in a rate description)

--- src/clib/rate_utils.cpp
+++ src/clib/rate_utils.cpp
@@ -45,35 +45,36 @@ enum { UNDEFINED_RATE_ID_ = 0 };
 
 // introduce some basic machinery to help us implement dynamic lookup of rates
 
-struct rateprop_ {
+/// A description of a rate
+struct Descr {
   double* data;
   const char* name;
 };
 
-static inline rateprop_ mk_rateprop_(double* rate, const char* name) {
-  rateprop_ out;
+static inline Descr mk_Descr(double* rate, const char* name) {
+  Descr out;
   out.data = rate;
   out.name = name;
   return out;
 }
 
-static inline rateprop_ mk_rateprop_standard_kcol_(
-    chemistry_data_storage* my_rates, const char* name, int index) {
+static inline Descr mk_Descr_standard_kcol_(chemistry_data_storage* my_rates,
+                                            const char* name, int index) {
   if ((my_rates == nullptr) || (my_rates->opaque_storage == nullptr) ||
       (my_rates->opaque_storage->kcol_rate_tables == nullptr)) {
-    return mk_rateprop_(nullptr, name);
+    return mk_Descr(nullptr, name);
   } else {
-    return mk_rateprop_(my_rates->opaque_storage->kcol_rate_tables->data[index],
-                        name);
+    return mk_Descr(my_rates->opaque_storage->kcol_rate_tables->data[index],
+                    name);
   }
 }
 
-#define MKPROP_(PTR, NAME)                                                     \
-  mk_rateprop_(((PTR) == nullptr) ? nullptr : (PTR)->NAME, #NAME)
-#define MKPROP_SCALAR_(PTR, NAME)                                              \
-  mk_rateprop_(((PTR) == nullptr) ? nullptr : &((PTR)->NAME), #NAME)
-#define MKPROP_STANDARD_KCOL_(PTR, NAME, INDEX)                                \
-  mk_rateprop_standard_kcol_(PTR, #NAME, INDEX)
+#define MKDESCR_(PTR, NAME)                                                    \
+  mk_Descr(((PTR) == nullptr) ? nullptr : (PTR)->NAME, #NAME)
+#define MKDESCR_SCALAR_(PTR, NAME)                                             \
+  mk_Descr(((PTR) == nullptr) ? nullptr : &((PTR)->NAME), #NAME)
+#define MKDESCR_STANDARD_KCOL_(PTR, NAME, INDEX)                               \
+  mk_Descr_standard_kcol_(PTR, #NAME, INDEX)
 
 // Create machinery to lookup Standard-Form Collisional Reaction Rates
 // -------------------------------------------------------------------
@@ -88,17 +89,16 @@ static inline rateprop_ mk_rateprop_standard_kcol_(
 //   of the previous value (if a value isn't explicitly specified)
 // - CollisionalRxnLUT::NUM_ENTRIES specifies the number of other enumeration
 //   constants (excluding CollisionalRxnLUT::NUM_ENTRIES) in the enum
-static rateprop_ get_CollisionalRxn_rateprop_(chemistry_data_storage* my_rates,
-                                              int i) {
+static Descr get_CollisionalRxn_Descr(chemistry_data_storage* my_rates, int i) {
   switch (i) {
 #define ENTRY(NAME)                                                            \
   case CollisionalRxnLUT::NAME: {                                              \
-    return MKPROP_STANDARD_KCOL_(my_rates, NAME, CollisionalRxnLUT::NAME);     \
+    return MKDESCR_STANDARD_KCOL_(my_rates, NAME, CollisionalRxnLUT::NAME);    \
   }
 #include "collisional_rxn_rate_members.def"
 #undef ENTRY
     default: {
-      rateprop_ out = {nullptr, nullptr};
+      Descr out = {nullptr, nullptr};
       return out;
     }
   }
@@ -121,32 +121,31 @@ enum MiscRxnRateKind_ {
   MiscRxn_NRATES  // <- will hold the number of reactions
 };
 
-static rateprop_ get_MiscRxn_rateprop_(chemistry_data_storage* my_rates,
-                                       int i) {
+static Descr get_MiscRxn_Descr(chemistry_data_storage* my_rates, int i) {
   switch (i) {
     // density dependent version of k13 (which is a CollisionalRxn)
     case MiscRxn_k13dd:
-      return MKPROP_(my_rates, k13dd);
+      return MKDESCR_(my_rates, k13dd);
     // Radiative rates for 6-species (for external field):
     case MiscRxn_k24:
-      return MKPROP_SCALAR_(my_rates, k24);
+      return MKDESCR_SCALAR_(my_rates, k24);
     case MiscRxn_k25:
-      return MKPROP_SCALAR_(my_rates, k25);
+      return MKDESCR_SCALAR_(my_rates, k25);
     case MiscRxn_k26:
-      return MKPROP_SCALAR_(my_rates, k26);
+      return MKDESCR_SCALAR_(my_rates, k26);
     // Radiative rates for 9-species
     case MiscRxn_k27:
-      return MKPROP_SCALAR_(my_rates, k27);
+      return MKDESCR_SCALAR_(my_rates, k27);
     case MiscRxn_k28:
-      return MKPROP_SCALAR_(my_rates, k28);
+      return MKDESCR_SCALAR_(my_rates, k28);
     case MiscRxn_k29:
-      return MKPROP_SCALAR_(my_rates, k29);
+      return MKDESCR_SCALAR_(my_rates, k29);
     case MiscRxn_k30:
-      return MKPROP_SCALAR_(my_rates, k30);
+      return MKDESCR_SCALAR_(my_rates, k30);
     case MiscRxn_k31:
-      return MKPROP_SCALAR_(my_rates, k31);
+      return MKDESCR_SCALAR_(my_rates, k31);
     default: {
-      rateprop_ out = {nullptr, nullptr};
+      Descr out = {nullptr, nullptr};
       return out;
     }
   }
@@ -155,47 +154,47 @@ static rateprop_ get_MiscRxn_rateprop_(chemistry_data_storage* my_rates,
 // define some additional generic machinery
 // ----------------------------------------
 
-#define RATE_SET_COUNT 2
-typedef rateprop_ fetch_rateprop_fn(chemistry_data_storage*, int);
-struct rateprop_set_ {
+#define DESCR_SET_COUNT 2
+typedef Descr fetch_Descr_fn(chemistry_data_storage*, int);
+struct Descr_set_ {
   int id_offset;
   int len;
-  fetch_rateprop_fn* fn;
+  fetch_Descr_fn* fn;
 };
 struct rate_registry_type_ {
   int len;
-  struct rateprop_set_ sets[RATE_SET_COUNT];
+  Descr_set_ sets[DESCR_SET_COUNT];
 };
 
 static const struct rate_registry_type_ rate_registry_ = {
-    /* len: */ RATE_SET_COUNT,
+    /* len: */ DESCR_SET_COUNT,
     /* sets: */ {
-        {1000, CollisionalRxnLUT::NUM_ENTRIES, &get_CollisionalRxn_rateprop_},
-        {2000, MiscRxn_NRATES, &get_MiscRxn_rateprop_}}};
+        {1000, CollisionalRxnLUT::NUM_ENTRIES, &get_CollisionalRxn_Descr},
+        {2000, MiscRxn_NRATES, &get_MiscRxn_Descr}}};
 
 struct ratequery_rslt_ {
   grunstable_rateid_type rate_id;
-  rateprop_ prop;
+  Descr descr;
 };
 
-/// internal function to search for the rate_property i
+/// internal function to search for the rate description i
 ///
 /// We interpret i as rate_id, when use_rate_id is true. Otherwise, we just
-/// look for the ith rateprop (we introduce an artificial distinction between
-/// the 2 cases because we want to reserve the right to be able to change the
-/// relationship if it becomes convenient in the future)
-static struct ratequery_rslt_ query_rateprop_(chemistry_data_storage* my_rates,
-                                              long long i, bool use_rate_id) {
+/// look for the ith rate description (we introduce an artificial distinction
+/// between the 2 cases because we want to reserve the right to be able to
+/// change the relationship if it becomes convenient in the future)
+static struct ratequery_rslt_ query_Descr(chemistry_data_storage* my_rates,
+                                          long long i, bool use_rate_id) {
   int total_len = 0;  // <- we increment this as we go through the rates
 
   for (int set_idx = 0; set_idx < rate_registry_.len; set_idx++) {
-    const struct rateprop_set_ cur_set = rate_registry_.sets[set_idx];
+    const struct Descr_set_ cur_set = rate_registry_.sets[set_idx];
 
     const long long tmp = (use_rate_id) ? i - cur_set.id_offset : i - total_len;
     if ((tmp >= 0) && (tmp < cur_set.len)) {
       struct ratequery_rslt_ out;
       out.rate_id = tmp + cur_set.id_offset;
-      out.prop = cur_set.fn(my_rates, tmp);
+      out.descr = cur_set.fn(my_rates, tmp);
       return out;
     }
     total_len += cur_set.len;
@@ -217,10 +216,10 @@ extern "C" grunstable_rateid_type grunstable_ratequery_id(const char* name) {
   }
 
   for (int set_idx = 0; set_idx < rate_q::rate_registry_.len; set_idx++) {
-    const rate_q::rateprop_set_ cur_set = rate_q::rate_registry_.sets[set_idx];
+    const rate_q::Descr_set_ cur_set = rate_q::rate_registry_.sets[set_idx];
     for (int i = 0; i < cur_set.len; i++) {
-      rate_q::rateprop_ prop = cur_set.fn(nullptr, i);
-      if (std::strcmp(name, prop.name) == 0) {
+      rate_q::Descr descr = cur_set.fn(nullptr, i);
+      if (std::strcmp(name, descr.name) == 0) {
         return cur_set.id_offset + i;
       }
     }
@@ -232,7 +231,7 @@ extern "C" double* grunstable_ratequery_get_ptr(
     chemistry_data_storage* my_rates, grunstable_rateid_type rate_id) {
   namespace rate_q = grackle::impl::ratequery;
 
-  return rate_q::query_rateprop_(my_rates, rate_id, true).prop.data;
+  return rate_q::query_Descr(my_rates, rate_id, true).descr.data;
 }
 
 extern "C" const char* grunstable_ith_rate(
@@ -241,9 +240,9 @@ extern "C" const char* grunstable_ith_rate(
 
   const long long sanitized_i = (i < LLONG_MAX) ? (long long)i : -1;
   rate_q::ratequery_rslt_ tmp =
-      rate_q::query_rateprop_(nullptr, sanitized_i, false);
+      rate_q::query_Descr(nullptr, sanitized_i, false);
   if (out_rate_id != nullptr) {
     *out_rate_id = tmp.rate_id;
   }
-  return tmp.prop.name;
+  return tmp.descr.name;
 }
