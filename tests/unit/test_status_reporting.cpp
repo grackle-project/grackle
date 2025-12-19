87e45fcc8a01c7e40cb3a1ca43640db5b8903a42
Matthew Abruzzo <matthewabruzzo@gmail.com>
2025-12-19 11:05:58 -0500

    start formatting test_status_reporting.cpp

--- tests/unit/test_status_reporting.cpp
+++ tests/unit/test_status_reporting.cpp
@@ -4,17 +4,15 @@
 #include <memory>  // unique_ptr
 #include <string>
 
-#include "grackle.h" // GR_FAIL
+#include "grackle.h"  // GR_FAIL
 #include "status_reporting.h"
 
 #include "grtestutils/os.hpp"
 
-
 TEST(StatusReportingTest, PrintErrMsg) {
-
   // setup capture of stderr
   std::unique_ptr<grtest::CaptureSink> capstderr =
-    grtest::CaptureSink::create(stderr);
+      grtest::CaptureSink::create(stderr);
   if (capstderr.get() == nullptr) {
     GTEST_SKIP() << "Stream redirection doesn't seem to be working. It may "
                  << "not be implemented for the current platform";
@@ -28,12 +26,10 @@ TEST(StatusReportingTest, PrintErrMsg) {
   EXPECT_THAT(msg, testing::HasSubstr("message with string: `str` & int: 4"));
 }
 
-
 TEST(StatusReportingTest, PrintAndReturnErr) {
-
   // setup capture of stderr
-  std::unique_ptr<grtest::CaptureSink> capstderr = 
-    grtest::CaptureSink::create(stderr);
+  std::unique_ptr<grtest::CaptureSink> capstderr =
+      grtest::CaptureSink::create(stderr);
   if (capstderr.get() == nullptr) {
     GTEST_SKIP() << "Stream redirection doesn't seem to be working. It may "
                  << "not be implemented for the current platform";
@@ -54,18 +50,18 @@ TEST(StatusReportingDeathTest, InternalError) {
   // right now, we are mostly just confirm that the program aborts
   // -> we are checking that the specified error is included
   // -> in the future, we have the option to test the formatting of the message
-  EXPECT_DEATH({
-    GR_INTERNAL_ERROR("This is a dummy error message %s", "dummy-str");
-  }, ".*This is a dummy error message dummy-str.*");
+  EXPECT_DEATH(
+      { GR_INTERNAL_ERROR("This is a dummy error message %s", "dummy-str"); },
+      ".*This is a dummy error message dummy-str.*");
 }
 
 TEST(StatusReportingDeathTest, InternalRequireFalse) {
   // right now, we are mostly just confirm that the program aborts
   // -> we are checking that the specified error is included
   // -> in the future, we have the option to test the formatting of the message
-  EXPECT_DEATH({
-    GR_INTERNAL_REQUIRE(1 == 0, "This is a dummy error message %d", 10);
-  }, ".*This is a dummy error message 10.*");
+  EXPECT_DEATH(
+      { GR_INTERNAL_REQUIRE(1 == 0, "This is a dummy error message %d", 10); },
+      ".*This is a dummy error message 10.*");
 }
 
 TEST(StatusReportingDeathTest, InternalRequireTrue) {
