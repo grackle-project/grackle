ca0cc0a177e4e739dc06c2303d7ff2d9dc9dd30e
Matthew Abruzzo <matthewabruzzo@gmail.com>
2026-01-12 11:25:41 -0500

    some more polishing of PartMap

--- src/clib/support/PartMap.hpp
+++ src/clib/support/PartMap.hpp
@@ -9,14 +9,19 @@
 /// Define/declare @ref PartMap
 ///
 //===----------------------------------------------------------------------===//
-#ifndef SUPPORT_PARTSEQ_HPP
-#define SUPPORT_PARTSEQ_HPP
+#ifndef SUPPORT_PARTMAP_HPP
+#define SUPPORT_PARTMAP_HPP
 #include "support/config.hpp"
+#include "support/FrozenKeyIdxBiMap.hpp"
 #include "status_reporting.h"
 
 namespace GRIMPL_NAMESPACE_DECL {
+namespace partmap_detail {
 /// we are starting with an arbitrarily low number
-inline constexpr int MAX_PART_SEQ_LEN = 4;
+inline constexpr int MAX_LEN = 4;
+}  // namespace partmap_detail
+
+using partition_descr_type = int;
 
 /// This type encodes a table of partitions
 ///
@@ -31,8 +36,16 @@ inline constexpr int MAX_PART_SEQ_LEN = 4;
 /// - Instances of this type exist to provide information about the indices
 ///   bounding a partition **AND** to find the partition containing an index
 ///
-/// For context, the following diagram sketches an example where an array of 9
-/// elements has been partitioned into 3 unequally sized partitions
+/// @par Basic Vocabulary
+/// To avoid confusion (especially with abbreviations):
+/// - an index is an index in array that is partitioned
+/// - the identifier of a partition is always a *partition descriptor* (and is
+///   abbreviated as `pd`)
+///
+/// @par Example
+/// Let's consider a simple example. This example breaks up a 9 element array
+/// into 3 partitions, PartitionName::A, PartitionName::B, & PartitionName::C,
+/// in a manner sketched below:
 ///
 /// @code{unparsed}
 ///        ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
@@ -40,16 +53,38 @@ inline constexpr int MAX_PART_SEQ_LEN = 4;
 ///        └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
 ///        ┗━━━━━━━━━━━┳━━━━━━━━━━━┻━━━━━┳━━━━━┻━━━━━━━━┳━━━━━━━━┛
 ///                    ┃                 ┃              ┃
-///               partition 0       partition 1    partition 2
-///                start: 0          start 4        start 6
-///                stop: 4           stop 6         stop 9
+/// PartitionName::A  ━┛                 ┃              ┃
+/// PartitionName::C  ━━━━━━━━━━━━━━━━━━━┛              ┃
+/// PartitionName::B  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 /// @endcode
 ///
-/// @par Basic Vocabulary
-/// To avoid confusion (especially with abbreviations):
-/// - an index is an index in array that is partitioned
-/// - the identifier of a partition is always a *partition descriptor* (and is
-///   abbreviated as `pd`)
+/// The names of the partitions are typically be declared as an enum (ideally,
+/// they would be "scoped enums," but that is tricky for reasons explained
+/// later). The declaration might look like:
+///
+/// @code{C++}
+/// namespace PartitionName { enum {A, B, C}; }
+/// @endcode
+///
+/// The code that constructs the enum would look something like:
+///
+/// @code{C++}
+///   /* Construct the partition map */
+///   int pds[3] = {PartitionName::A, PartitionMap::C, PartitionMap::B};
+///   int size[3] = {4, 2, 3};
+///   PartMap m = new_PartitionMap(pds, sizes, 3);
+///   if (!PartitionMap_is_ok(&m)) { /* <error-propagation ...> */ }
+///
+///   /* query the bounds associated with PartitionMap::C */
+///   IdxInterval bounds = PartMap_part_bounds(&m, PartitionMap::C);
+///   assert(bounds.start == 4);
+///   assert(bounds.stop == 6); /
+///
+///   /* query the partition associated with index 7 */
+///   partmap::IdxSearch search_rslt = PartMap_search_idx(&m, 7);
+///   assert(search_rslt.has_val);
+///   assert(search_rslt.pd == PartitionName::B);
+/// @endcode
 ///
 /// @par Motivation
 /// The concept modeled by this datatype models is **EXTREMELY** common in
@@ -76,46 +111,130 @@ inline constexpr int MAX_PART_SEQ_LEN = 4;
 ///    queries of field category or ranges of keys in a given category (in
 ///    particular: chemical-species-fields, dust-species fields, injection
 ///    path metal density, etc.)
+///
+/// @par Ideas for improvement
+/// There are 2 ideas:
+/// 1. Make this machinery compatible with "scoped enums."
+///    - For context, regular C-style enums implicitly converts to and from
+///      integer datatypes, whereas scoped enums (aka "class enums") require
+///      explicit casts. This produces nice behaviors:
+///      1. if a function argument has type `E`, where `E` is a scoped enum,
+///         the compiler reports an error if you try to pass anything other
+///         than an enumerator declared within the declaration of `E`.
+///      2. if you try to pass an enumerator that was declared as part of a
+///         scoped enum, `E`, to a function argument with a type other than
+///         `E`, the compiler will report an error.
+///    - To make this machinery compatible with scoped enums, we would need to
+///      make PartMap into a template struct where `partition_descr_type` is a
+///      template parameter
+/// 2. Better Performance: We can almost always assume a particular ordering
+///    of the partition descriptors.
+///    - There are a few ways we can take advantage of this.
+///    - It's probably wise to hold off on this until we start using this type
+///      in a bunch of places and performance is a demonstrated issue (I'm a
+///      little skeptical, since this probably won't be used deep within any
+///      nested loops)
+///
+/// @note
+/// The contents of this struct should be considered an implementation
+/// detail! Always prefer the associated functions (they are defined in such
+/// a way that they should be inlined)
 struct PartMap {
   /// number of partitions
   int n_parts;
+  /// the list of partition descriptors associated with each partition
+  partition_descr_type pd_array[partmap_detail::MAX_LEN];
   /// the upper bounds on each partition
-  int right_idx_bounds[MAX_PART_SEQ_LEN];
+  int right_idx_bounds[partmap_detail::MAX_LEN];
 };
+
+namespace partmap_detail {
+// helper function (that I wish didn't exist)
+inline PartMap mk_invalid() {
+  PartMap out;
+  out.n_parts = -1;
+  for (int i = 0; i < partmap_detail::MAX_LEN; i++) {
+    out.pd_array[i] = 0;
+    out.right_idx_bounds[i] = 0;
+  }
+  return out;
+}
+}  // namespace partmap_detail
 
 /// Construct a PartMap from the sizes of each partition.
 ///
-/// @param[in] part_sizes Holds the number of indices for each partition.
+/// @param[in] pds Array of unique partition descriptors
+/// @param[in] sizes Holds the number of indices for each partition.
 /// @param[in] n_parts The number of partitions
-inline PartMap PartMap_from_part_sizes(const int* part_sizes, int n_parts) {
-  if (part_sizes == nullptr && n_parts == 0) {
+///
+/// @note
+/// Callers should pass the returned value to @ref PartMap_isok
+/// to check whether there was an error during creation. This is pretty
+/// ugly/clunky, but it's the only practical way to achieve comparable behavior
+/// to other internal data types. The best alternatives involve things like
+/// std::optional or converting this type to a simple C++ class.
+inline PartMap new_PartMap(const partition_descr_type* pds, const int* sizes,
+                           int n_parts) {
+  if (n_parts == 0) {
     PartMap out;
     out.n_parts = n_parts;
+    out.pd_array[0] = -1;
     out.right_idx_bounds[0] = 0;
     return out;
   }
 
-  // todo: gracefully handle errors
   // (in reality, any error here points to an internal logic-error)
-  if (part_sizes == nullptr) {
-    GR_INTERNAL_ERROR("part_sizes is nullptr when n_parts isn't 0");
-  } else if (n_parts == 0) {
-    GR_INTERNAL_ERROR("n_parts is 0 when part_sizes isn't nullptr");
-  } else if (n_parts < 0 || n_parts > MAX_PART_SEQ_LEN) {
-    GR_INTERNAL_ERROR("n_parts doesn't satisfy 0 <= n_parts <= %d",
-                      MAX_PART_SEQ_LEN);
+  if (pds == nullptr || sizes == nullptr) {
+    GrPrintErrMsg("pds and sizes can only be a nullptr when n_parts is 0");
+    return partmap_detail::mk_invalid();
+  } else if (n_parts < 0 || n_parts > partmap_detail::MAX_LEN) {
+    GrPrintErrMsg("n_parts doesn't satisfy 0 <= n_parts <= %d",
+                  partmap_detail::MAX_LEN);
   }
 
   PartMap out;
   out.n_parts = n_parts;
   int running_sum = 0;
-  for (int part_id = 0; part_id < n_parts; part_id++) {
-    GR_INTERNAL_REQUIRE(part_sizes[part_id] >= 0,
-                        "partition size can't be negative");
-    running_sum += part_sizes[part_id];
-    out.right_idx_bounds[part_id] = running_sum;
+  for (int i = 0; i < n_parts; i++) {
+    // error checks:
+    for (int j = 0; j < i; j++) {
+      if (pds[i] == pds[j]) {
+        GrPrintErrMsg("pds[%d] and pds[%d] hold the same descriptor", i, j);
+        return partmap_detail::mk_invalid();
+      }
+    }
+    if (sizes[i] < 0) {
+      GrPrintErrMsg("sizes[%d] is negative", i);
+      return partmap_detail::mk_invalid();
+    }
+
+    out.pd_array[i] = pds[i];
+    running_sum += sizes[i];
+    out.right_idx_bounds[i] = running_sum;
   }
   return out;
+}
+
+/// checks whether a creational function produced a valid partition map
+///
+/// @param[in] ptr Points to the partition map being checked
+/// @return true if the value is ok or false if the value is invalid
+///
+/// @important
+/// The interface of @ref PartMap sets values in a very particular way to
+/// signal that an instance is in an invalid state. This function @b ONLY
+/// checks for that particular signature.
+inline bool PartMap_is_ok(const PartMap* ptr) { return (ptr->n_parts >= 0); }
+
+/// Destroys the internal data tracked by an instance
+///
+/// @param[in] ptr A non-null pointer to a valid partition map
+///
+/// @note
+/// The @b ONLY exists for consistency with other data types. (It's
+/// unnecessary to call this function)
+inline void drop_PartMap(PartMap* ptr) {
+  return;  // NO-OP
 }
 
 /// number of partitions in the partition map
@@ -136,40 +255,71 @@ struct IdxInterval {
 ///
 /// @param[in] m Valid pointer to a partition map
 /// @param[in] pd The partition descriptor to query
-inline IdxInterval PartMap_part_bounds(const PartMap* m, int pd) {
-  if (pd < 0 || pd >= m->n_parts) {
-    return IdxInterval{-1, -1};
+inline IdxInterval PartMap_part_bounds(const PartMap* m,
+                                       partition_descr_type pd) {
+  // simple, stupid, linear search
+  for (int i = 0; i < m->n_parts; i++) {
+    if (pd == m->pd_array[i]) {
+      return IdxInterval{/*start=*/(i == 0) ? 0 : m->right_idx_bounds[i - 1],
+                         /*stop=*/m->right_idx_bounds[i]};
+    }
   }
-  return IdxInterval{
-      /*start=*/(pd == 0) ? 0 : m->right_idx_bounds[pd - 1],
-      /*stop=*/m->right_idx_bounds[pd]};
+  return IdxInterval{-1, -1};
 }
 
+namespace partmap {
 /// holds info pertaining to the partition holding an index
-struct IdxPartSearch {
+struct IdxSearch {
   /// indicates whether the index was found
-  bool is_valid;
+  bool has_val;
+
+  /// The index being searched
+  ///
+  /// @note
+  /// This is primarily tracked in order to make the key_partition_search
+  /// convenience function provide more useful results
+  int index;
+
   /// the partition descriptor
-  int pd;
+  partition_descr_type pd;
+
   /// offset of the index relative to the start of the partition
   int start_offset;
 };
+}  // namespace partmap
 
 /// search for the partition containing an index
 ///
 /// @param[in] m Valid pointer to a partition map
 /// @param[in] idx The index to search for
-inline IdxPartSearch PartMap_search_idx(const PartMap* m, int idx) {
-  if (idx >= 0) {  // simple, stupid, linear search
-    for (int pd = 0; pd < m->n_parts; pd++) {
-      if (idx < m->right_idx_bounds[pd]) {
-        int part_start = (pd == 0) ? 0 : m->right_idx_bounds[pd - 1];
-        return IdxPartSearch{true, pd, idx - part_start};
+inline partmap::IdxSearch PartMap_search_idx(const PartMap* m, int idx) {
+  if (idx >= 0) {
+    // simple, stupid, linear search
+    for (int i = 0; i < m->n_parts; i++) {
+      if (idx < m->right_idx_bounds[i]) {
+        int part_start = (i == 0) ? 0 : m->right_idx_bounds[i - 1];
+        return partmap::IdxSearch{true, idx, m->pd_array[i], idx - part_start};
       }
     }
   }
-  return {false, -1, -1};
+  return {false, idx, m->pd_array[0], -1};
+}
+
+/// search for the partition containing @p key
+///
+/// This is a convenience function that can be used when the keys of @p bimap
+/// are partitioned according to @p part_map
+///
+/// @param[in] part_map Describes the partitioning of indices
+/// @param[in] bimap Describes bi-directional mapping between indices & keys
+/// @param[in] key The key to that is being searched
+inline partmap::IdxSearch key_partition_search(const PartMap* part_map,
+                                               FrozenKeyIdxBiMap* bimap,
+                                               const char* key) {
+  bimap::AccessRslt maybe_idx = FrozenKeyIdxBiMap_find(bimap, key);
+  return PartMap_search_idx(part_map,
+                            maybe_idx.has_value ? -1 : maybe_idx.value);
 }
 }  // namespace GRIMPL_NAMESPACE_DECL
 
-#endif  // SUPPORT_PARTSEQ_HPP
+#endif  // SUPPORT_PARTMAP_HPP
--- tests/unit/support/test_unit_PartMap.cpp
+++ tests/unit/support/test_unit_PartMap.cpp
@@ -9,12 +9,26 @@
 /// test the PartMap data type
 ///
 //===----------------------------------------------------------------------===//
-
+#include <iostream>  // needed to teach googletest how to print
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
 #include "support/config.hpp"
 #include "support/PartMap.hpp"
+
+// teach GoogleTest how to print GRIMPL_NS::partmap::IdxSearch for more
+// informative errors (otherwise it just shows the memory's raw byte values)
+namespace GRIMPL_NS::partmap {
+void PrintTo(const IdxSearch& search, std::ostream* os) {
+  bool valid = search.has_val;
+  std::string index = (valid) ? std::to_string(search.index) : "<garbage>";
+  std::string pd = (valid) ? std::to_string(search.pd) : "<garbage>";
+  std::string start_offset =
+      (valid) ? std::to_string(search.start_offset) : "<garbage>";
+  *os << "{has_val=" << valid << ", index=" << index << ", pd=" << pd
+      << ", start_offset=" << start_offset << '}';
+}
+}  // namespace GRIMPL_NS::partmap
 
 using ::testing::Eq;
 using ::testing::Field;
@@ -22,60 +36,88 @@ using ::testing::Lt;
 
 // this is a simple case
 TEST(PartSeq, Empty) {
-  GRIMPL_NS::PartMap s = GRIMPL_NS::PartMap_from_part_sizes(nullptr, 0);
+  GRIMPL_NS::PartMap m = GRIMPL_NS::new_PartMap(nullptr, nullptr, 0);
+  ASSERT_TRUE(grackle::impl::PartMap_is_ok(&m));
 
-  EXPECT_EQ(GRIMPL_NS::PartMap_n_partitions(&s), 0);
-  EXPECT_EQ(GRIMPL_NS::PartMap_n_idx(&s), 0);
+  EXPECT_EQ(GRIMPL_NS::PartMap_n_partitions(&m), 0);
+  EXPECT_EQ(GRIMPL_NS::PartMap_n_idx(&m), 0);
 
   EXPECT_THAT(
-      GRIMPL_NS::PartMap_part_bounds(&s, 0),
+      GRIMPL_NS::PartMap_part_bounds(&m, 0),
       ::testing::AllOf(Field("start", &GRIMPL_NS::IdxInterval::start, Lt(0)),
                        Field("stop", &GRIMPL_NS::IdxInterval::stop, Lt(0))));
 
-  using GRIMPL_NS::IdxPartSearch;
-  EXPECT_THAT(
-      GRIMPL_NS::PartMap_search_idx(&s, 0),
-      Field("is_valid", &IdxPartSearch::is_valid, Eq(false)));
+  using GRIMPL_NS::partmap::IdxSearch;
+  EXPECT_THAT(GRIMPL_NS::PartMap_search_idx(&m, 0),
+              Field("has_val", &IdxSearch::has_val, Eq(false)));
 }
+
+// these act as the names of the partition descriptors that are used in the
+// following test-case
+//
+// Ideally, these would be scoped-enums, but that makes use of PartMap very
+// clunky! (The only effective way to use a scoped-enum is to make PartMap
+// a class template, where partition_descr
+namespace PartitionName {
+enum { A, B, C };
+}  // namespace PartitionName
 
 // this is the case illustrated in PartMap's docstring
 TEST(PartSeq, DocString) {
-  const int part_sizes[3] = {4, 2, 3};
+  const int pds[3] = {PartitionName::A, PartitionName::C, PartitionName::B};
+  const int sizes[3] = {4, 2, 3};
 
-  GRIMPL_NS::PartMap s = GRIMPL_NS::PartMap_from_part_sizes(part_sizes, 3);
+  GRIMPL_NS::PartMap m = GRIMPL_NS::new_PartMap(pds, sizes, 3);
+  ASSERT_TRUE(grackle::impl::PartMap_is_ok(&m));
 
-  EXPECT_EQ(GRIMPL_NS::PartMap_n_partitions(&s), 3);
-  EXPECT_EQ(GRIMPL_NS::PartMap_n_idx(&s), 9);
+  EXPECT_EQ(GRIMPL_NS::PartMap_n_partitions(&m), 3);
+  EXPECT_EQ(GRIMPL_NS::PartMap_n_idx(&m), 9);
 
   EXPECT_THAT(
-      GRIMPL_NS::PartMap_part_bounds(&s, 0),
+      GRIMPL_NS::PartMap_part_bounds(&m, PartitionName::A),
       ::testing::AllOf(Field("start", &GRIMPL_NS::IdxInterval::start, Eq(0)),
                        Field("stop", &GRIMPL_NS::IdxInterval::stop, Eq(4))));
   EXPECT_THAT(
-      GRIMPL_NS::PartMap_part_bounds(&s, 1),
+      GRIMPL_NS::PartMap_part_bounds(&m, PartitionName::C),
       ::testing::AllOf(Field("start", &GRIMPL_NS::IdxInterval::start, Eq(4)),
                        Field("stop", &GRIMPL_NS::IdxInterval::stop, Eq(6))));
   EXPECT_THAT(
-      GRIMPL_NS::PartMap_part_bounds(&s, 2),
+      GRIMPL_NS::PartMap_part_bounds(&m, PartitionName::B),
       ::testing::AllOf(Field("start", &GRIMPL_NS::IdxInterval::start, Eq(6)),
                        Field("stop", &GRIMPL_NS::IdxInterval::stop, Eq(9))));
 
-  using GRIMPL_NS::IdxPartSearch;
+  using GRIMPL_NS::partmap::IdxSearch;
   EXPECT_THAT(
-      GRIMPL_NS::PartMap_search_idx(&s, 2),
-      ::testing::AllOf(
-          Field("is_valid", &IdxPartSearch::is_valid, Eq(true)),
-          Field("pd", &IdxPartSearch::pd, Eq(0)),
-          Field("start_offset", &IdxPartSearch::start_offset, Eq(2))))
-      << "index 2 should be in the partition with part_id = 0 & start_offset "
+      GRIMPL_NS::PartMap_search_idx(&m, 2),
+      ::testing::AllOf(Field("has_val", &IdxSearch::has_val, Eq(true)),
+                       Field("index", &IdxSearch::index, Eq(2)),
+                       Field("pd", &IdxSearch::pd, Eq(PartitionName::A)),
+                       Field("start_offset", &IdxSearch::start_offset, Eq(2))))
+      << "index 2 should be in the partition with pd = PartitionName::A & "
+         "start_offset "
       << "should be 2";
 
   EXPECT_THAT(
-      GRIMPL_NS::PartMap_search_idx(&s, 6),
-      ::testing::AllOf(
-          Field("is_valid", &IdxPartSearch::is_valid, Eq(true)),
-          Field("pd", &IdxPartSearch::pd, Eq(2)),
-          Field("start_offset", &IdxPartSearch::start_offset, Eq(0))))
-      << "index 2 should be in the partition with part_id = 0 & start_offset "
-      << "should be 2";
+      GRIMPL_NS::PartMap_search_idx(&m, 5),
+      ::testing::AllOf(Field("has_val", &IdxSearch::has_val, Eq(true)),
+                       Field("index", &IdxSearch::index, Eq(5)),
+                       Field("pd", &IdxSearch::pd, Eq(PartitionName::C)),
+                       Field("start_offset", &IdxSearch::start_offset, Eq(1))))
+      << "index 5 should be in the partition with pd = PartitionName::C & "
+         "start_offset "
+      << "should be 1";
+
+  EXPECT_THAT(
+      GRIMPL_NS::PartMap_search_idx(&m, 6),
+      ::testing::AllOf(Field("has_val", &IdxSearch::has_val, Eq(true)),
+                       Field("index", &IdxSearch::index, Eq(6)),
+                       Field("pd", &IdxSearch::pd, Eq(PartitionName::B)),
+                       Field("start_offset", &IdxSearch::start_offset, Eq(0))))
+      << "index 6 should be in the partition with pd = PartitionName::B & "
+         "start_offset "
+      << "should be 0";
+
+  // extra sanity check!
+  EXPECT_THAT(GRIMPL_NS::PartMap_search_idx(&m, 9999),
+              Field("is_valid", &IdxSearch::has_val, Eq(false)));
 }
