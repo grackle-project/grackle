37732297ba3dbe422cd81b48c5fb4394dd931798
Matthew Abruzzo <matthewabruzzo@gmail.com>
2026-01-16 18:25:18 -0500

    add a docstring for dust_related_props

--- src/clib/dust/misc.hpp
+++ src/clib/dust/misc.hpp
@@ -24,8 +24,46 @@ namespace grackle::impl {
 
 /// Calculate various dust related properties
 ///
+/// @todo
+/// - Stop passing @p cool1dmulti_buf and instead pass the `mynh` and
+///   `gasgr_tdust` members directly.
+///   - The former is unmodified while the latter is used as a scratch buffer
+///   - to minimize conflicts wait for the `calc_all_tdust_gasgr_1d_g`
+///     transcription PR
+/// - Make all non-modified `double*` arguments `const double` (this must wait
+///   until after the transcription PRs for the wrapped fortran routines)
+///
 /// @param[in] anydust Whether dust chemistry is enabled
-/// @param[in] tgas gas temperature
+/// @param[in] tgas 1d array of gas temperature
+/// @param[in] itmask Specifies the general iteration-mask of the @p idx_range
+///     for this calculation.
+/// @param[in] itmask_metal Specifies the metal/dust-specific iteration-mask of
+///     the @p idx_range for this calculation.
+/// @param[in] my_chemistry holds a number of configuration parameters.
+/// @param[in] my_rates Holds assorted rate data and other internal
+///     configuration info.
+/// @param[in] my_fields Specifies the field data.
+/// @param[in] internalu Specifies Grackle's internal unit-system
+/// @param[in] idx_range Specifies the current index-range
+/// @param[in] logTlininterp_buf hold values for each location in @p idx_range
+///     that are used to linearly interpolate tables with respect to the
+///     natural log of @p tgas.
+/// @param[in,out] cool1dmulti_buf The mynh member holds a 1D array of number
+///     densities while the gasgr_tdust member is just a scratch buffer
+/// @param[in] comp2 Holds the CMB temperature at the current redshift
+/// @param[out] dust2gas Holds the computed dust-to-gas ratio at each
+///     location in the index range. In other words, this holds the dust mass
+///     per unit gas mass (only used in certain configuration)
+/// @param[in,out] myisrf a scratch buffer that may be used to temporarily
+///     record the interstellar radiation field
+/// @param[out] tdust, grain_temperatures dust temperatures may be written
+///     to one of these variables, based on configuration
+/// @param[out] gasgr, gas_grainsp_heatrate Grain/gas energy transfer rates may
+///     be written to one of these variables, based on configuration
+/// @param[out] grain_kappa, kappa_tot Opacity-related information may be
+///     written to one of these variables, based on configuration
+/// @param[in,out] internal_dust_prop_buf Holds scratch-space for holding
+///     grain-specific information
 inline void dust_related_props(
     gr_mask_type anydust, double* tgas, double* metallicity,
     const gr_mask_type* itmask, gr_mask_type* itmask_metal,
