d9c76db8774da5c5644954f4d55529706850db58
ChristopherBignamini <christopher.bignamini@gmail.com>
2026-04-08 11:16:32 +0200

    Documentation update and variable renaming

--- src/clib/dust/calc_tdust_1d_g.hpp
+++ src/clib/dust/calc_tdust_1d_g.hpp
@@ -38,12 +38,12 @@ namespace grackle::impl {
 /// @param[in]  isrf Interstellar radiation field strength in Habing units
 /// @param[in]  itmask  Itaration mask
 /// @param[in]  trad CMB ratiation temperature
-/// @param[in]  in Length of the 1D slice
+/// @param[in]  buf_len Length of the 1D slice
 /// @param[in]  gr_N Number of temperature points in the grain opacity table
 /// @param[in]  gr_dT Temperature spacing of the grain opacity table
 /// @param[in]  gr_Td Temperature values of the grain opacity table
 /// @param[in]  alsp_data_ Grain opacity table data
-/// @param[in]  kgr Array to hold computed grain opacities
+/// @param[out] kgr Array to hold computed grain opacities
 /// @param[in]  idspecies Array of grain species IDs (only used in certain
 /// configurations)
 /// @param[in]  idx_range Index range specifying the portion of the grid to
@@ -54,7 +54,7 @@ namespace grackle::impl {
 /// modified: January, 2026 by Christopher Bignamini & Matthew Abruzzo; C++ port
 void calc_tdust_1d_g(double* tdust, double* tgas, double* nh, double* gasgr,
                      const double* gamma_isrfa, const double* isrf,
-                     const gr_mask_type* itmask, double trad, int in, int gr_N,
+                     const gr_mask_type* itmask, double trad, int buf_len, int gr_N,
                      double* gr_dT, double* gr_Td, gr_float* alsp_data_,
                      double* kgr, int* idspecies, IndexRange idx_range);
 
