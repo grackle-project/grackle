d41c38826e30443edfa03231aaa272c7e41bdf52
Matthew Abruzzo <matthewabruzzo@gmail.com>
2026-05-01 20:55:54 -0400

    remove duplicate definition of GR_INTERNAL_UNREACHABLE_ERROR

--- src/clib/support/status_reporting.hpp
+++ src/clib/support/status_reporting.hpp
@@ -249,23 +249,6 @@ ERRFMT_ATTR_(2) [[noreturn]] void abort_with_internal_err_(
   }
 
 
-/// @def GR_INTERNAL_UNREACHABLE_ERROR()
-/// @brief function-like macro that aborts with a (lethal) error message
-///     indicating that 
-///
-/// This macro should be treated as a function with the signature:
-///
-///   [[noreturn]] void GR_INTERNAL_UNREACHABLE_ERROR();
-///
-/// @note
-/// Unlike gcc/clang's __builtin_unreachable or C++23's std::unreachable, this
-/// aborts the program with an error if its executed (the other cases produce
-/// undefined behavior). (An argument could be made for conditionally compiling
-/// this macro into the alternatives to test speed)
-#define GR_INTERNAL_UNREACHABLE_ERROR()                                        \
-  { ::GRIMPL_NS::abort_with_internal_err_(__GRIMPL_SRCLOC__,                   \
-                                          "location shouldn't be reachable"); }
-
 namespace GRIMPL_NAMESPACE_DECL {
 // helper function
 ERRFMT_ATTR_(2) [[nodiscard]] int print_and_return_err_(SourceLocation locinfo,
