80a33754851ab969b9ac03578407b66643c0d019
Matthew Abruzzo <matthewabruzzo@gmail.com>
2026-05-02 09:54:17 -0400

    Convert build_index_helper_ to an inline function
    The main motivation for doing this was to get rid of the
    index_helper.cpp file. We can always revert this choice later

--- src/clib/support/index_helper.hpp
+++ src/clib/support/index_helper.hpp
@@ -169,7 +169,36 @@ inline IndexRange make_idx_range_(
   return out;
 }
 
-grackle_index_helper build_index_helper_(const grackle_field_data *my_fields);
+/// @brief Construct a new @ref grackle_index_helper
+///
+/// @note
+/// This function is only declared `inline` as a matter of convenience. If it
+/// would help compile-times, we could always move the definition to a source
+/// file
+inline grackle_index_helper build_index_helper_(
+    const grackle_field_data *my_fields) {
+  grackle_index_helper out;
+  const int rank = my_fields->grid_rank;
+
+  // handle i indices
+  out.i_dim   = my_fields->grid_dimension[0];
+  out.i_start = my_fields->grid_start[0];
+  out.i_end   = my_fields->grid_end[0];
+
+  // handle j indices (j_end isn't tracked by grackle_index_helper)
+  out.j_dim   = (rank >= 2) ? my_fields->grid_dimension[1] : 1;
+  out.j_start = (rank >= 2) ? my_fields->grid_start[1]     : 0;
+  int j_end   = (rank >= 2) ? my_fields->grid_end[1]       : 0;
+  out.num_j_inds = (j_end - out.j_start) + 1;
+
+  // handle k indices (k_end & k_dim aren't tracked by grackle_index_helper)
+  out.k_start = (rank >= 3) ? my_fields->grid_start[2]     : 0;
+  int k_end   = (rank >= 3) ? my_fields->grid_end[2]       : 0;
+  int num_k_inds = (k_end - out.k_start) + 1;
+
+  out.outer_ind_size = num_k_inds * out.num_j_inds;
+  return out;
+}
 
 #ifdef __cplusplus
 } // extern "C"
