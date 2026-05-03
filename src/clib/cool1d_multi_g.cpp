04ce3af520e3a0549fcb8f2df97b961ed61720d3
Matthew Abruzzo <matthewabruzzo@gmail.com>
2026-05-03 19:34:25 -0400

    explicitly zero-out alpha_continuum

--- src/clib/cool1d_multi_g.cpp
+++ src/clib/cool1d_multi_g.cpp
@@ -812,6 +812,11 @@ void grackle::impl::cool1d_multi_g(
     }
   }
 
+  // zero-out the continuum absorption coefficients
+  for (i = idx_range.i_start; i <= idx_range.i_end; i++) {
+    alpha_continuum[i] = 0.0;
+  }
+
   dust_related_props(anydust, tgas, cool1dmulti_buf.mynh, metallicity, itmask,
                      itmask_metal, my_chemistry, my_rates, my_fields, internalu,
                      idx_range, logTlininterp_buf, comp2, dust2gas, tdust,
@@ -841,13 +846,6 @@ void grackle::impl::cool1d_multi_g(
                                interp_grid.props.data_size, interp_grid.data);
 
         alpha_continuum[i] = std::pow(1.e1, log_a);
-      }
-    }
-
-  } else {
-    for (i = idx_range.i_start; i <= idx_range.i_end; i++) {
-      if (itmask[i] != MASK_FALSE) {
-        alpha_continuum[i] = 0.f;
       }
     }
   }
