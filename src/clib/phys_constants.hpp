cfc91e1842f14bcf3ee571a4a4a4635b8facbd77
Matthew Abruzzo <matthewabruzzo@gmail.com>
2026-08-16 10:21:01 -0400

    adjust comment format for overview docstrings

--- src/clib/phys_constants.hpp
+++ src/clib/phys_constants.hpp
@@ -1,41 +1,64 @@
-/***********************************************************************
-/
-/ Physical constants
-/
-/
-/ Copyright (c) 2013, Enzo/Grackle Development Team.
-/
-/ Distributed under the terms of the Enzo Public Licence.
-/
-/ The full license is in the file LICENSE, distributed with this 
-/ software.
-************************************************************************/
+//===----------------------------------------------------------------------===//
+//
+// See the LICENSE file for license and copyright information
+// SPDX-License-Identifier: NCSA AND BSD-3-Clause
+//
+//===----------------------------------------------------------------------===//
+///
+/// @file
+/// Describes physical constants (in CGS units).
+///
+/// @par History
+/// This file comes from the phys_constants.h header file in Enzo that was
+/// originally written by Elizabeth Tasker (May, 2005) and later renamed by
+/// Daniel Reynolds
+///
+//===----------------------------------------------------------------------===//
 
 #ifndef PHYS_CONSTANTS_HPP
 #define PHYS_CONSTANTS_HPP
-/***********************************************************************
-/  
-/ DEFINITION OF PHYSICAL CONSTANTS
-/  
-/ written by: Elizabeth Tasker (renamed by Daniel Reynolds)
-/ date:       May, 2005 
-/
-/ Note: CGS units
-/
-/ Historically, all constants in this file always expanded to floating point
-/ values of type `double`.
-/ - In constrast, the "phys_const.def" fortran header always defined
-/   macro-constants that expand to floating-point values of type `gr_float`.
-/ - To aide with transcribing code from Fortran to C/C++ we have
-/   defined versions of most constants in this file that expand to constant
-/   of type `gr_float`. These constants have the `_grflt` suffix.
-/ - In the future, it would be nice to do away with the alternative versions
-/   of these constants
-/
-*********************************************************************/
 
 #include "grackle_float.h"
 #include "grackle_macros.h" // GRFLOAT_C
+
+
+/// @defgroup PhysConsts Physical Constants
+///
+/// This group of entities specify Physical Constants (in CGS units)
+///
+/// Historically, all constants in this file always expanded to floating point
+/// values of type `double`.
+/// - In constrast, the "phys_const.def" fortran header always defined
+///   macro-constants that expand to floating-point values of type `gr_float`.
+/// - To aide with transcribing code from Fortran to C/C++ we have
+///   defined versions of most constants in this file that expand to constant
+///   of type `gr_float`. These constants have the `_grflt` suffix.
+///
+/// Ideas for the future:
+/// =====================
+/// - It would be nice to do away with the alternative versions of these
+///   constants
+/// - It would be nice to convert all constants from MACROS convert to c++
+///   constants (reminiscent of the style of the <numbers> header from C++ 20)
+///   and put them inside a dedicated namespace (perhaps GRIMPL_NS::constant?)
+///   - it's generally considered better practice to use named constants than
+///     macros (it can certainly simplify things with debuggers)
+///   - For example, this might look like:
+///     @code{.cpp}
+///     namespace GRIMPL_NAMESPACE_DECL {
+///     namespace constant{
+///
+///     /// Boltzmann's constant [cm^2 * g * s^-2 * K^-1] or [erg / K] 
+///     inline constexpr double kboltz = 1.3806504e-16;
+///     inline constexpr gr_float kboltz_grflt = kboltz;
+///
+///     /// Mass of hydrogen [g]
+///     inline constexpr double mh 1.67262171e-24;
+///     inline constexpr gr_float mh_grflt = mh;
+///     }
+///     }
+/** @{*/
+
 
 /* Physics constants */
 
@@ -108,5 +131,7 @@
 #define hplanck_grflt  GRFLOAT_C(6.6260693e-27)
 #define ev2erg_grflt   GRFLOAT_C(1.60217653e-12)
 #define sigma_sb_grflt GRFLOAT_C(5.670373e-5)
+
+/** @}*/ // end of group
 
 #endif  // PHYS_CONSTANTS_HPP
